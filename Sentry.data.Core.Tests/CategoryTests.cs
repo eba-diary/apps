f78df769b5053d4cea88ebd86009ad9b3c030ec1
Gosse Jered <Jered.Gosse@sentry.com>
2017-05-16 21:05:18 +0000

    Created CategoryTests Class

---
+++ Sentry.data.Core.Tests/CategoryTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sentry.data.Core;
+
+namespace Sentry.data.Core.Tests
+{
+    [TestClass]
+    public class CategoryTests
+    {
+
+        [TestCategory("CategoryTests")]
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
