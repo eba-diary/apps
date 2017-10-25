05e41fa7ef0155ba700be0ce7f86a3990afb6371
Gosse Jered <Jered.Gosse@sentry.com>
2017-10-25 12:57:18 +0000

    Adding AssetSource Table

---
+++ Sentry.data.Database/Tables/AssetSource.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[AssetSource]
+(
+	[Source_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SourceDisplay_NME] VARCHAR(50) NULL, 
+    [Source_DSC] VARCHAR(100) NULL, 
+    [MetadataRepositorySrcSys_VAL] VARCHAR(50) NOT NULL, 
+    [DataAsset_ID] INT NOT NULL, 
+    [IsVisible_IND] BIT NOT NULL, 
+    CONSTRAINT [FK_AssetSource_DataAsset] FOREIGN KEY ([DataAsset_ID]) REFERENCES [DataAsset]([DataAsset_ID]) 
+)
