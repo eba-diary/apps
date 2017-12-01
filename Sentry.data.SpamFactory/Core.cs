d347f9141fe4f30613dfda68416c786d18217aec
Quaschnick Andrew <Andrew.Quaschnick@sentry.com>
2017-12-01 22:24:37 +0000

    SpamFactory

---
+++ Sentry.data.SpamFactory/Core.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading;
+using Sentry.Common.Logging;
+using Sentry.Configuration;
+using System.Threading.Tasks;
+using System;
+
+
+namespace Sentry.data.SpamFactory
+{
+    /// <summary>
+    /// This class represents where you should run your application logic
+    /// </summary>
+    public class Core
+    {
+
+        private CancellationTokenSource _tokenSource;
+        private CancellationToken _token;
+
+        /// <summary>
+        /// Start the core worker
+        /// </summary>
+        public void OnStart()
+        {
+            Logger.Info("Windows Service starting...");
+
+            //setup a token to allow a task to be cancelled
+            _tokenSource = new CancellationTokenSource();
+            _token = _tokenSource.Token;
+
+            //start up a new task
+            Task.Factory.StartNew(this.DoWork, TaskCreationOptions.LongRunning).ContinueWith(TaskException, TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        /// <summary>
+        /// Loop until cancellation is requested, doing the primary work of this service
+        /// </summary>
+        private void DoWork()
+        {
+            Logger.Info("Worker task started.");
+
+            do
+            {
+                if(DateTime.Now.Minute == 0)
+                {
+                    Task.Factory.StartNew(() => Watch.Hourly());
+                }
+
+                if(DateTime.Now.Hour == 12)
+                {
+                    Task.Factory.StartNew(() => Watch.Daily());
+                }
+
+                if(DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+                {
+                    Task.Factory.StartNew(() => Watch.Weekly());
+                }
+
+
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+
+
+
+            } while (!_token.IsCancellationRequested);
+            Logger.Info("Worker task stopped.");
+        }
+
+        /// <summary>
+        /// Request the core worker to stop
+        /// </summary>
+        public void OnStop()
+        {
+            Logger.Info("Windows Service stopping...");
+            //request the task to cancel itself
+            _tokenSource.Cancel();
+        }
+
+        /// <summary>
+        /// An exception occurred on the main task; log it and shut down the service with an error exit code
+        /// </summary>
+        /// <param name="t">The main thread task</param>
+        private void TaskException(Task t)
+        {
+            Logger.Fatal("Exception occurred on main Windows Service Task. Stopping Service immediately.", t.Exception);
+            Environment.Exit(10001);
+        }
+
+    }
+}
---
+++ Sentry.data.SpamFactory/Watch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentry.data.SpamFactory
+{
+    class Watch
+    {
+
+        public static void Run()
+        {
+
+
+
+
+
+
+            Task.Factory.StartNew(() => Hourly());
+
+
+        }
+
+        public static void Hourly()
+        {
+            //Get all events in the last hour
+
+
+        }
+
+        public static void Daily()
+        {
+            //Get all events in the last day
+
+
+
+        }
+
+        public static void Weekly()
+        {
+            //Get all events in the last week
+
+
+
+        }
+
+
+
+    }
+}
