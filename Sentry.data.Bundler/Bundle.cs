1bff8928c78ec35a57ac42f9f558ec1e4bcd96a9
Gosse Jered <Jered.Gosse@sentry.com>
2018-01-10 18:31:17 +0000

    Adjusting orderby

--- Sentry.data.Bundler/Bundle.cs
+++ Sentry.data.Bundler/Bundle.cs
@@ -196,8 +196,8 @@ namespace Sentry.data.Bundler
 
             //Split original part list in half
             //If odd number second half will have remainer
-            firstHalf = parts_list.OrderByDescending(o => o.Id).Take(partcnt/2).ToList();
-            secondHalf = parts_list.OrderByDescending(o => o.Id).Skip(partcnt / 2).ToList();
+            firstHalf = parts_list.OrderBy(o => o.Id).Take(partcnt/2).ToList();
+            secondHalf = parts_list.OrderBy(o => o.Id).Skip(partcnt / 2).ToList();
             //object b = new Semaphore(1,10)
 
             try
@@ -318,8 +318,8 @@ namespace Sentry.data.Bundler
         {
             int partcnt = list.Count();
             bool remainerProcessed = false;
-            List<BundlePart> firstHalf = list.Take(partcnt / 2).ToList();
-            List<BundlePart> secondHalf = list.Skip(partcnt / 2).ToList();   
+            List<BundlePart> firstHalf = list.OrderBy(o => o.Id).Take(partcnt / 2).ToList();
+            List<BundlePart> secondHalf = list.OrderBy(o => o.Id).Skip(partcnt / 2).ToList();   
             Parallel.ForEach(firstHalf, (part) =>
             {
                 //int secondId = ((partcnt / 2) - 2) + part.Id;
