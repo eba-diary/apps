e7762071e64fcc8557f223a2c4b15e9d4feec722
Jeff Sojka <jeff.sojka@sentry.com>
2019-01-14 12:58:39 -0600

    Changes made to the Business Intelligence area to update the format of the sentry owner name and also put back the update frequency value but change the label to refresh frequency

---
+++ Sentry.data.Core/Helpers/DisplayFormatter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sentry.Associates;
+
+namespace Sentry.data.Core.Helpers
+{
+    /// <summary>
+    /// Provides common area for any text formatting (i.e. phone numbers, dates, associate name)
+    /// </summary>
+    /// 
+    public static class DisplayFormatter
+    {
+        public static string FormatAssociateName(Associate sentryAssociate)
+        {
+            // determine whether to use the associate's first or familiar name
+            var assocName = (string.IsNullOrWhiteSpace(sentryAssociate.FamiliarName)) ? sentryAssociate.FirstName : sentryAssociate.FamiliarName;
+
+            // tack on a space and the associate's last name
+            assocName += " " + sentryAssociate.LastName;
+
+            return assocName;
+        }
+    }
+}
