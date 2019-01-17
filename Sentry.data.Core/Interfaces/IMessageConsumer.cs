fab6ac50c05191752cc81e0befd86910ac6dd805
Jered Gosse <jered.gosse@sentry.com>
2019-01-17 12:52:02 -0600

    CLA-112 - Adding kafkaConsumer within infrastructure

---
+++ Sentry.data.Core/Interfaces/IMessageConsumer.cs
@@ -0,0 +1,9 @@
+﻿namespace Sentry.data.Core
+{
+    public interface IMessageConsumer
+    {
+        void Close();
+        void Open();
+        void RequestStop();
+    }
+}
--- Sentry.data.Infrastructure/ServiceImplementations/KafkaConsumer.cs
+++ Sentry.data.Infrastructure/ServiceImplementations/KafkaConsumer.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using Confluent.Kafka;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Confluent.Kafka;
+using Sentry.data.Core;
+using Sentry.Common.Logging;
 
-namespace Sentry.data.Infrastructure.ServiceImplementations
+namespace Sentry.data.Infrastructure
 {
-    public class KafkaConsumer1
+    public abstract class BaseKafkaConsumer<keyT> : IMessageConsumer, IDisposable
     {
+        #region Declarations
+        private readonly string _groupId;
+        private readonly string _bootstrapServers;
+        private readonly List<string> _topicNames;
+        private readonly string _environment;
+        private  Consumer<keyT, String> _consumer;
+        
+
+        bool _stop = false;
+        #endregion
+
+        //public event OnMessageReadyHandler MessageReady;
+
+
+        protected BaseKafkaConsumer(string groupId, string bootstrapServers, 
+            IList<string> topicNames, string environment)
+        {
+            _groupId = groupId;
+            _bootstrapServers = bootstrapServers;
+            _topicNames = topicNames.ToList();
+            _environment = environment;
+        }
         //public void Init()
         //{
         //    var conf = new Confluent.Kafka.Consumer.ConsumerConfig
@@ -26,5 +50,75 @@ namespace Sentry.data.Infrastructure.ServiceImplementations
 
         //    Confluent.Kafka.
         //}
+
+        void IMessageConsumer.Close()
+        {
+            if (_consumer != null) _consumer.Dispose();
+        }
+
+        void IMessageConsumer.Open()
+        {
+            IList<KeyValuePair<string, Object>> configuration = new List<KeyValuePair<String, Object>>()
+            {
+                {new KeyValuePair<string, object>("group.id", _groupId) },
+                { new KeyValuePair<String, Object>("bootstrap.servers", _bootstrapServers) },
+                { new KeyValuePair<String, Object>("enable.auto.commit", "true") },
+                { new KeyValuePair<String, Object>("auto.commit.interval.ms", "500") },
+                //{ new KeyValuePair<String, Object>("statistics.interval.ms", "60000") }
+                { new KeyValuePair<String, Object>("default.topic.config", new Dictionary<string, object>()
+                    {
+                        { "auto.offset.reset", "smallest" }
+                    })
+                }
+            };
+
+            _consumer = new Confluent.Kafka.Consumer<keyT, String>(configuration, this.GetKeyDeserializer(), new Confluent.Kafka.Serialization.StringDeserializer(Encoding.UTF8));
+
+            _consumer.OnMessage += _consumer_OnMessage;
+
+            _consumer.Subscribe(_topicNames);
+
+            Task.Factory.StartNew(() =>
+            {
+                while (!_stop)
+                {
+                    _consumer.Poll(100);
+                }
+            });
+
+        }
+
+        void IMessageConsumer.RequestStop()
+        {
+            try
+            {
+                //Unassign the partitions
+                _consumer.Unassign();
+            }
+            catch (Exception e)
+            {
+                Logger.Info(e.Message);
+            }
+
+            _stop = true;
+        }
+
+        //public void ProcessMessage(string msg)
+        //{
+        //    MessageReady(this, msg);
+        //}
+
+        protected abstract void _consumer_OnMessage(object sender, Message<keyT, string> e);
+        protected abstract Confluent.Kafka.Serialization.IDeserializer<keyT> GetKeyDeserializer();
+
+        //public static void Init(string applicationName)
+        //{
+        //    //Confluent.Kafka. 
+        //}
+        
+        public void Dispose()
+        {
+            if (_consumer != null) _consumer.Dispose();
+        }
     }
 }
