8a69cab58c5bba3f8de3c8a9a64cca5bbe16959e
Jered Gosse <jered.gosse@sentry.com>
2019-01-18 11:14:58 -0600

    CLA-112 Adding additional project from CustomerOne to support messaging

--- Sentry.data.Core/Interfaces/IMessageConsumer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sentry.data.Core
-{
-    public interface IMessageConsumer
-    {
-        void Close();
-        void Open();
-        void RequestStop();
-    }
-}
--- StandaloneLibraries/Sentry.Messaging.Common/TransactionTracking/Implementations/MessageTransactionLite.cs
+++ StandaloneLibraries/Sentry.Messaging.Common/TransactionTracking/Implementations/MessageTransactionLite.cs
@@ -1,20 +1,20 @@
-﻿using Sentry.DataRetentionPolicy;
+﻿//using Sentry.DataRetentionPolicy;
 using System;
 
 namespace Sentry.Messaging.Common
 {
-    public class MessageTransactionLite : BaseTransaction, ISummarizable
+    public class MessageTransactionLite : BaseTransaction//, ISummarizable
     {
         public string OriginalMessageId { get;  set; }
         public string MessageDetailedType { get;  set; }
         public virtual DateTime MessageDate { get;  set; }
         public bool IsSummarized { get; set; }
 
-        string ISummarizable.DataId { get { return Id.ToString(); } set { Id = Guid.Parse(value); } }
-        DateTime ISummarizable.DataDate { get { return MessageDate; } set { MessageDate = value; } }
-        string ISummarizable.JoiningId { get { return OriginalMessageId; } set { OriginalMessageId = value; } }
-        string ISummarizable.SplitFieldValue { get { return MessageDetailedType; } set { MessageDetailedType = value; } }
-        bool ISummarizable.IsSummarized { get { return IsSummarized; } set { IsSummarized = value; } }
+        //string ISummarizable.DataId { get { return Id.ToString(); } set { Id = Guid.Parse(value); } }
+        //DateTime ISummarizable.DataDate { get { return MessageDate; } set { MessageDate = value; } }
+        //string ISummarizable.JoiningId { get { return OriginalMessageId; } set { OriginalMessageId = value; } }
+        //string ISummarizable.SplitFieldValue { get { return MessageDetailedType; } set { MessageDetailedType = value; } }
+        //bool ISummarizable.IsSummarized { get { return IsSummarized; } set { IsSummarized = value; } }
 
         public MessageTransactionLite(Guid id, DateTime messageDate, string originalMessageId, string messageDetailedType, bool isSummarized)
         {
