3c1926ed54674dd112f14073f70c02d0ac3f8d65
Dan Zirnhelt <Dan.Zirnhelt@sentry.com>
2019-01-25 09:43:07 -0600

    Update to the dacpac to change char to varchar.

--- Sentry.data.Database/Scripts/Post-Deploy/SupportingScripts/Sprint_19_2_1/InsertDatasetCategories.sql
+++ Sentry.data.Database/Scripts/Post-Deploy/SupportingScripts/Sprint_19_2_1/InsertDatasetCategories.sql
@@ -6,7 +6,9 @@
 INSERT INTO DatasetCategory (Dataset_Id, Category_Id)
 SELECT Dataset_ID, Category_ID FROM #TempDatasetCategory
 
-UPDATE Dataset SET Dataset_TYP = 'DS' WHERE Dataset_TYP IS NULL;
+UPDATE Dataset SET Dataset_TYP = 'DS' WHERE Dataset_TYP IS NULL OR Dataset_TYP = 'DS ';
+UPDATE Category SET Object_TYP = 'DS' WHERE Object_TYP = 'DS ';
+
 
 If(OBJECT_ID('tempdb..#TempDatasetCategory') Is Not Null)
 Begin
