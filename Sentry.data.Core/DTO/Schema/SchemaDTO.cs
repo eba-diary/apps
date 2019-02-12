48f96611c21bf33b59cd8ed38976a75371e83034
Jered Gosse <jered.gosse@sentry.com>
2019-02-12 14:26:13 -0600

    CLA-291 Adding HiveLocation, changed Header to be exposed as string via API

--- Sentry.data.Core/DTO/Schema/SchemaDTO.cs
+++ Sentry.data.Core/DTO/Schema/SchemaDTO.cs
@@ -1,13 +1,16 @@
-﻿namespace Sentry.data.Core
+﻿using System;
+
+namespace Sentry.data.Core
 {
     public class SchemaDTO
     {
         public int SchemaID { get; set; }
         public string Format { get; set; }
-        public bool Header { get; set; }
+        public string Header { get; set; }
         public string Delimiter { get; set; }
         public string HiveTable { get; set; }
         public string HiveDatabase { get; set; }
         public string HiveStatus { get; set; }
+        public string HiveLocation { get; set; }
     }
 }
