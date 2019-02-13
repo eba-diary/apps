b05834dca00da6ac42a618d2ea0f199d211d3384
Jered Gosse <jered.gosse@sentry.com>
2019-02-13 12:58:42 -0600

    Un-Commenting scripts so they run in production.

--- Sentry.data.Database/Scripts/Pre-Deploy/SupportingScripts/Sprint_19_2_1/InsertCategoriesToTemp.sql
+++ Sentry.data.Database/Scripts/Pre-Deploy/SupportingScripts/Sprint_19_2_1/InsertCategoriesToTemp.sql
@@ -1,7 +1,7 @@
 ﻿--This will pull the categories off of the dataset table and insert a link into a temp table.
 --a post deploy script will then insert the records from the temp table into the real datasetCategory Table once it is created.
 
---CREATE table #TempDatasetCategory ( Dataset_Id int, Category_Id int)
+CREATE table #TempDatasetCategory ( Dataset_Id int, Category_Id int)
 
---INSERT into #TempDatasetCategory (Dataset_Id, Category_Id)
---Select Dataset_ID, Category_ID from Dataset
+INSERT into #TempDatasetCategory (Dataset_Id, Category_Id)
+Select Dataset_ID, Category_ID from Dataset
