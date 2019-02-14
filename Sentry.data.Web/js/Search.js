0fed736f9b5b0013a13b5e544174fec0c6aa1055
Jeff Sojka <jeff.sojka@sentry.com>
2019-02-14 09:04:20 -0600

    CLA-290. Moved some javascript from the view to the js file

--- Sentry.data.Web/js/Search.js
+++ Sentry.data.Web/js/Search.js
@@ -4,66 +4,113 @@
 
 data.Search = {
 
-    Init: function (searchParms) {
+    Init: function () {
         /// <summary>
         /// Initialization function run at first load
         /// </summary>
-        /// <param name="searchParms">Should include the parameters from the model:
-        /// - SearchText
-        /// - SearchState
-        /// - SearchCategory
-        /// - SearchPage
-        /// </param>
-        searchParms.SearchPage = 1;
-        data.Search.LoadResults(searchParms);
 
-        $("#PagerPrevious").on("click", function (e) {
+        window.vm = new ListViewModel();
+        ko.applyBindings(vm);
+
+        if (window.location.href.match(/&ids=/)) {
+            // do nothing
+        }
+        else {
+            window.vm.clearFilters();
+        }
+
+        $(document).on("click", "[id^='filterType_']", function (e) {
             e.preventDefault();
-            if (searchParms.SearchPage <= 1) { return false; }
-            searchParms.SearchPage = searchParms.SearchPage - 1;
-            data.Search.LoadResults(searchParms);
-            return false;
+
+            var id = $(this).attr("id");
+            var category = "#hide_" + id
+            var icon = "#icon_" + id;
+
+            $(category).slideToggle();
+            $(icon).toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
         });
 
-        $("#PagerNext").on("click", function (e) {
+        $(document).on("click", "[id^='filterMore_']", function (e) {
             e.preventDefault();
-            if ($("#EndRecordNumber").text() == $("#TotalRecordCount").text()) { return false; }
-            searchParms.SearchPage = searchParms.SearchPage + 1;
-            data.Search.LoadResults(searchParms);
-            return false;
+
+            var id = $(this).attr("id");
+            var show = "#hidden_" + id
+            var icon = "#icon_" + id;
+            var txt = "#txt_" + id;
+
+            $(show).slideToggle();
+            $(icon).toggleClass("glyphicon-plus-sign glyphicon-minus-sign");
+
+            if ($(txt).text() == "Show Less") {
+                $(txt).text("Show More");
+            }
+            else {
+                $(txt).text("Show Less");
+            }
         });
+
+        $(document).on("click", ".btnFavorite", function (e) {
+            e.preventDefault();
+
+            var button = $(this);
+            var id = button.attr("data");
+
+            if (button.hasClass("glyphicon-star")) {
+
+                $.ajax({
+                    url: '/Favorites/SetFavorite?datasetId=' + encodeURIComponent(id),
+                    method: "GET",
+                    dataType: 'json',
+                    success: function () {
+                    },
+                    error: function () {
+                        Sentry.ShowModalAlert("Failed to remove favorite.");
+                    }
+                });
+            }
+            else if (button.hasClass("glyphicon-star-empty")) {
+
+                $.ajax({
+                    url: '/Favorites/SetFavorite?datasetId=' + encodeURIComponent(id),
+                    method: "GET",
+                    dataType: 'json',
+                    success: function () {
+                    },
+                    error: function () {
+                        Sentry.ShowModalAlert("Failed to set favorite.");
+                    }
+                });
+            }
+
+            $(this).toggleClass("glyphicon-star glyphicon-star-empty");
+
+        });
+        
+
+        // remove the "container" class from the parent object so the fluid container takes affect
+        $("#search-pg-wrapper").parent().removeClass("container");
+
+        // mouseover effect to change the background color of the search results tile
+        $(".ul-dataset-list-item").on("mouseenter", function () {
+            $(this).css('background-color', $(this).css("border-color").slice(0, -1) + ', 0.1)');
+        });
+
+        // mouseout effect to change the background color of the search results tile back to white
+        $(".ul-dataset-list-item").on("mouseleave", function () {
+            $(this).css('background-color', 'white');
+        });
+
     },
 
-    LoadResults: function (searchParms) {
-        /// <summary>
-        /// Asynchronously load a page of search results into the #SearchResults div
-        /// </summary>
-        /// <param name="searchParms">
-        /// The searchParms object should include the following properties:
-        /// - SearchText
-        /// - SearchState
-        /// - SearchCategory
-        /// - SearchPage
-        /// </param>
-        Sentry.InjectSpinner($("#SearchResults"), 200);
-        $.get("/Search/ResultList/" + searchParms.SearchText + "?SearchState=" + searchParms.SearchState + "&SearchCategory=" + searchParms.SearchCategory + "&SearchPage=" + searchParms.SearchPage, function (data) {
-            if (searchParms.SearchPage <= 1) {
-                $(".previous").addClass("disabled");
-            } else {
-                $(".previous").removeClass("disabled");
-            }
-            $("#SearchResults").html(data);
-            if ($("#EndRecordNumber").text() == $("#TotalRecordCount").text()) {
-                $(".next").addClass("disabled");
-            } else {
-                $(".next").removeClass("disabled");
-            }
-            if ($("#SearchResults a").first().length > 0) {
-                //make the "Next" button in the callout match the URL of the first asset in the listing
-                $("#next").attr("href", $("#SearchResults a").first().attr("href"));
-            } else {
-                $("#next").hide();
-            }
-        });
+    GetDetailType: function () {
+        // get the detail type from the URL
+        return window.location.pathname.split("/")[2];
+    },
+
+    GetSearchType: function () {
+        var regex = /\/Search\/(\w+)/,
+            url = window.location.href;
+        return regex.exec(url)[1];
     }
-}
+    
+};
