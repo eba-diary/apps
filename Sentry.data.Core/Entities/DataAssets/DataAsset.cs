04d2d15490fc3c7056a742415a869fff8f173b0e
Dan Zirnhelt <Dan.Zirnhelt@sentry.com>
2019-02-22 11:16:23 -0600

    CLA-242 - update dataAsset class

--- Sentry.data.Core/Entities/DataAssets/DataAsset.cs
+++ Sentry.data.Core/Entities/DataAssets/DataAsset.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sentry.data.Core
 {
     public class DataAsset : ISecurable
     {
+        private List<DataAssetHealth> _assetHealth;
 
         public DataAsset()
         {
@@ -33,5 +35,43 @@ namespace Sentry.data.Core
         public virtual string PrimaryOwnerId { get; set; }
         public virtual string PrimaryContactId { get; set; }
         public virtual Security Security { get; set; }
+
+
+
+
+        public virtual List<DataAssetHealth> AssetHealth
+        {
+            get
+            {
+                if (_assetHealth == null)
+                {
+                    _assetHealth = MetadataRepositoryService.GetByAssetName(MetadataRepAssetName, AssetSource.Where(w => w.IsVisiable).ToList()).OrderBy(o => o.SourceSystem).ToList();
+                }
+                return _assetHealth;
+            }
+        }
+        public virtual Boolean HealthIncludesSourceSystems
+        {
+            get
+            {
+                if (this.AssetHealth.Count > 1 || (this.AssetHealth.Count == 1 && this.AssetHealth.FirstOrDefault().SourceSystem != ""))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public virtual DateTime MaxProcessTime
+        {
+            get
+            {
+                if (this.AssetHealth.Count > 0)
+                {
+                    return this.AssetHealth.Max(m => m.AssetUpdtDTM);
+                }
+                return DateTime.MinValue;
+            }
+        }
+
     }
 }
