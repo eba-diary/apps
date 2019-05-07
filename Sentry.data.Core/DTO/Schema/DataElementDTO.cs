6318388f626d1618893450e89dd43d3688daf5d5
Jered Gosse <Jered.Gosse@sentry.com>
2019-05-07 10:00:22 -0500

    CLA-492 - removed schema delimiter validation on dataset edit, added schema validation on dataset create, added schema validation on schema edit, removed schema add, adjusted wording for schema heading.  CLA-491 - corrected Create\Edit headings for dataset.

---
+++ Sentry.data.Core/DTO/Schema/DataElementDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentry.data.Core
+{
+    public class DataElementDto
+    {
+        public string SchemaName { get; set; }
+        public string SchemaDescription { get; set; }
+        public bool SchemaIsForceMatch { get; set; }
+        public bool SchemaIsPrimary { get; set; }
+        public string Delimiter { get; set; }
+        public DateTime DataElementChange_DTM { get; set; }
+        public bool HasHeader { get; set; }
+        public int FileFormatId { get; set; }
+    }
+}
