795a5c6d6a223701beadebce42c4d4e1c5b979f2
Jered Gosse <jered.gosse@sentry.com>
2019-05-09 09:17:26 -0500

    CLA-266 - correcting create page error, removing old files

--- Sentry.data.Web/Controllers/NotificationController.cs
+++ Sentry.data.Web/Controllers/NotificationController.cs
@@ -31,7 +31,7 @@ namespace Sentry.data.Web.Controllers
         }
 
         [HttpGet]
-        public ActionResult ModifyNotification(int notificationId)
+        public ActionResult ModifyNotification(int notificationId = 0)
         {
             if (_notificationService.CanUserModifyNotifications())
             {
