5cca0f45519cb86fc783cc80a93134300a8b9a92
Jered Gosse <jered.gosse@sentry.com>
2019-07-16 10:14:59 -0500

    CLA-648 - removed identity column from authenticationtype table

--- Sentry.data.Database/Tables/RetrieverJobs/AuthenticationType.sql
+++ Sentry.data.Database/Tables/RetrieverJobs/AuthenticationType.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[AuthenticationType]
 (
-	[Auth_Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Auth_Id] INT NOT NULL PRIMARY KEY, 
     [AuthType_CDE] NVARCHAR(250) NOT NULL, 
     [Display_NME] NVARCHAR(250) NOT NULL, 
     [Description] NVARCHAR(250) NOT NULL
