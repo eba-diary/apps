e0c611d9b8c4e6f0c710252c5bab9a2e8047562b
Jered Gosse <jered.gosse@sentry.com>
2019-07-22 10:54:39 -0500

    CLA-607 - added DatasetFileReply table

---
+++ Sentry.data.Database/Tables/DatasetFileReply.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DatasetFileReply]
+(
+	[DatasetFileReply_Id] [int] IDENTITY(1,1) NOT NULL,
+                [DatasetFile_Id] [int] NOT NULL,
+                [Schema_ID] [int] NULL,
+                [FileLocation] [varchar](250) NOT NULL,
+                [ReplyStatus] [varchar](250) NOT NULL
+
+PRIMARY KEY CLUSTERED 
+(
+                [DatasetFileReply_Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
