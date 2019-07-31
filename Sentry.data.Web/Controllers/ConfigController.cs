82823e23a3cd2cc8381b9f4350c51f0071040c7b
Jered Gosse <jered.gosse@sentry.com>
2019-07-31 08:55:27 -0500

    CLA-717 - filtering out DFSBasicHSZ from dropdown lists

--- Sentry.data.Web/Controllers/ConfigController.cs
+++ Sentry.data.Web/Controllers/ConfigController.cs
@@ -458,7 +458,8 @@ namespace Sentry.data.Web.Controllers
             cjm.SourceTypesDropdown = temp.Where(x => 
                 x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_DROP_LOCATION && 
                 x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_S3_DROP_LOCATION && 
-                x.Value != GlobalConstants.DataSoureDiscriminator.JAVA_APP_SOURCE).OrderBy(x => x.Value);
+                x.Value != GlobalConstants.DataSoureDiscriminator.JAVA_APP_SOURCE &&
+                x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_HSZ_DROP_LOCATION).OrderBy(x => x.Value);
 
             List<SelectListItem> temp2 = new List<SelectListItem>();
 
@@ -782,9 +783,10 @@ namespace Sentry.data.Web.Controllers
             });
 
             csm.SourceTypesDropdown = temp.Where(x => 
-                    x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_DROP_LOCATION && 
-                    x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_S3_DROP_LOCATION && 
-                    x.Value != GlobalConstants.DataSoureDiscriminator.JAVA_APP_SOURCE).OrderBy(x => x.Value);
+                x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_DROP_LOCATION && 
+                x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_S3_DROP_LOCATION && 
+                x.Value != GlobalConstants.DataSoureDiscriminator.JAVA_APP_SOURCE &&
+                x.Value != GlobalConstants.DataSoureDiscriminator.DEFAULT_HSZ_DROP_LOCATION).OrderBy(x => x.Value);
 
             if (csm.SourceType == null)
             {
