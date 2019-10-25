556e1c09a0c77b7e2a2d8f76cca86d96d494046f
Jered Gosse <Jered.Gosse@sentry.com>
2019-10-25 09:06:47 -0500

    CLA-960, fixed logExecution on dataflow

--- Sentry.data.Core/Entities/DataProcessing/DataFlow.cs
+++ Sentry.data.Core/Entities/DataProcessing/DataFlow.cs
@@ -24,21 +24,21 @@ namespace Sentry.data.Core.Entities.DataProcessing
         public virtual IList<DataFlow_Log> Logs { get; set; }
         public virtual DataFlow_Log LogExecution(string executionGuid, string log, Log_Level level, Exception ex = null)
         {
-            string logMsg = $"{executionGuid}{((runInstanceGuid != null) ? "-" + runInstanceGuid : String.Empty)} {log}";
+            string logMsg = $"{executionGuid} {log}";
             switch (level)
             {
                 case Log_Level.Info:
-                    Logger.Info(log);
+                    Logger.Info(logMsg);
                     break;
                 case Log_Level.Warning:
-                    Logger.Warn(log);
+                    Logger.Warn(logMsg);
                     break;
                 case Log_Level.Debug:
-                    Logger.Debug(log);
+                    Logger.Debug(logMsg);
                     break;
                 default:
                 case Log_Level.Error:
-                    Logger.Error(log, ex);
+                    Logger.Error(logMsg, ex);
                     break;
             }
 
