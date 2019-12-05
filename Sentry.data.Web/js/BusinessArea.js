e1a3c46bab1c763e19fcbba1eefaab012359e782
072186 <austin.laszewski@sentry.com>
2019-12-05 09:52:24 -0600

    checkpoint, toastr git added, BusinessArea.js added, toastr added to BundleConfig.cs

---
+++ Sentry.data.Web/js/BusinessArea.js
@@ -0,0 +1,29 @@
+﻿data.BusinessArea = {
+
+
+    PersonalLinesInit: function () {
+
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
+        toastr["error"]("Message Test", "Austin");
+
+        toastr["error"]("Message Test2", "Austin2");
+    }
+
+}
