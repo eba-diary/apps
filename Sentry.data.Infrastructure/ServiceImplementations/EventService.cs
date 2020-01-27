4c1f36337ac69f4973274d00e49ca9a9b0b86222
072186 <austin.laszewski@sentry.com>
2020-01-27 14:25:57 -0600

    CLA-1186 - Rename private methods in EventService pre-emptively prior to merge from Master to prepare for Jereds changes.

--- Sentry.data.Infrastructure/ServiceImplementations/EventService.cs
+++ Sentry.data.Infrastructure/ServiceImplementations/EventService.cs
@@ -14,7 +14,7 @@ namespace Sentry.data.Infrastructure
             IDatasetContext _datasetContext = Bootstrapper.Container.GetNestedContainer().GetInstance<IDatasetContext>();
             int datasetId = _datasetContext.GetById<DatasetFileConfig>(configId).ParentDataset.DatasetId;
             
-            Task.Factory.StartNew(() => PublishSuccessEvent(eventTypeDescription, userId, reason, datasetId, configId, 0), TaskCreationOptions.RunContinuationsAsynchronously);
+            Task.Factory.StartNew(() => SaveEvent(eventTypeDescription, userId, reason, datasetId, configId, 0), TaskCreationOptions.RunContinuationsAsynchronously);
         }
 
 
@@ -27,28 +27,28 @@ namespace Sentry.data.Infrastructure
             DatasetFileConfig dfc = (ds == null) ? null : ds.DatasetFileConfigs.FirstOrDefault();
             int configId = (dfc == null) ? 0 : dfc.ConfigId;
 
-            Task.Factory.StartNew(() => PublishSuccessEvent(eventTypeDescription, userId, reason, datasetId, configId, 0), TaskCreationOptions.RunContinuationsAsynchronously);
+            Task.Factory.StartNew(() => SaveEvent(eventTypeDescription, userId, reason, datasetId, configId, 0), TaskCreationOptions.RunContinuationsAsynchronously);
         }
 
         public void PublishSuccessEvent(string eventTypeDescription, string userId, string reason)
         {
-            Task.Factory.StartNew(() => PublishSuccessEvent(eventTypeDescription, userId, reason, 0, 0, 0), TaskCreationOptions.RunContinuationsAsynchronously);
+            Task.Factory.StartNew(() => SaveEvent(eventTypeDescription, userId, reason, 0, 0, 0), TaskCreationOptions.RunContinuationsAsynchronously);
         }
 
         public void PublishSuccessEventByNotificationId(string eventTypeDescription, string userId, string reason, int notificationId)
         {
-            Task.Factory.StartNew(() => PublishSuccessEvent(eventTypeDescription, userId, reason, 0, 0, notificationId), TaskCreationOptions.RunContinuationsAsynchronously);
+            Task.Factory.StartNew(() => SaveEvent(eventTypeDescription, userId, reason, 0, 0, notificationId), TaskCreationOptions.RunContinuationsAsynchronously);
         }
 
 
-        private void PublishSuccessEvent(string eventTypeDescription, string userId, string reason, int datasetId, int configId, int notificationId)
+        private void SaveEvent(string eventTypeDescription, string userId, string reason, int datasetId, int configId, int notificationId)
         {
             using (IDatasetContext _datasetContext = Bootstrapper.Container.GetNestedContainer().GetInstance<IDatasetContext>())
             {
                 EventType et = _datasetContext.EventTypes.Where(w => w.Description == eventTypeDescription).FirstOrDefault();
                 Status status = _datasetContext.EventStatus.Where(w => w.Description == GlobalConstants.Statuses.SUCCESS).FirstOrDefault();
 
-                Event evt = CreateAndSaveEvent(et, status, userId, reason, datasetId, configId, notificationId);
+                Event evt = CreateEvent(et, status, userId, reason, datasetId, configId, notificationId);
 
                 _datasetContext.Add(evt);
                 _datasetContext.SaveChanges();
@@ -56,7 +56,7 @@ namespace Sentry.data.Infrastructure
         }
 
         
-        private Event CreateAndSaveEvent(EventType eventType, Status status, string userId, string reason, int? datasetId, int? configId, int? notificationId)
+        private Event CreateEvent(EventType eventType, Status status, string userId, string reason, int? datasetId, int? configId, int? notificationId)
         {
             return new Event()
             {
