7aa0be112f753c5fe19e213edea80618f51933f0
072186 <austin.laszewski@sentry.com>
2020-02-25 15:27:59 -0600

    CLA-1394: checkpoint #1

--- Sentry.data.Web/js/BusinessArea.js
+++ Sentry.data.Web/js/BusinessArea.js
@@ -6,5 +6,34 @@
 
         //1=DATASET 2=BUSINESSAREA
         data.Subscription.init(2);
+    },
+
+    AlertNotificationHeroInit: function ()
+    {
+
+        $(".alert-notification-icon-hero").popover
+        (
+            {
+                content: "BEWARE THE IDES OF MARCH ssssssssssss sssssssssssssssssssss ssssssssssssssssssssssss ssssssssssssssssss"
+            }
+        )
+        
+
+        //pass initilize hmtl tag
+
+
+
+
+        //$(".alert-notification-hero").on("click", function (event)
+        //{
+        //    console.log("FIRE");
+        //    $(".alert-notification-hero").popover('show');
+
+        //    //$(".alert-panel-hero").show();
+           
+        //});
+
+       
     }
+
 };   
