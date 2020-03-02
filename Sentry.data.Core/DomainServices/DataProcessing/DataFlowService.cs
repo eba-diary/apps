7952f4a4fc32924ec3edd90c821b83a755f037a7
Jered Gosse <jered.gosse@sentry.com>
2020-03-02 14:54:48 -0600

    CLA-1433 - added DFS drop location to non-schema specific data flows defined as push

--- Sentry.data.Core/DomainServices/DataProcessing/DataFlowService.cs
+++ Sentry.data.Core/DomainServices/DataProcessing/DataFlowService.cs
@@ -230,6 +230,10 @@ namespace Sentry.data.Core
             //This type of dataflow does not need to worry about retrieving data from external sources
             // Data will be pushed by user to S3 and\or DFS drop locations
 
+            //Add default DFS drop location for data flow
+            RetrieverJob dfsDataFlowBasic = _jobService.InstantiateJobsForCreation(df, _datasetContext.DataSources.First(x => x.Name.Contains(GlobalConstants.DataSourceName.DEFAULT_DATAFLOW_DFS_DROP_LOCATION)));
+            _datasetContext.Add(dfsDataFlowBasic);
+
             //Generate ingestion steps (get file to raw location)
             AddDataFlowStep(dto, df, DataActionType.S3Drop);
 
