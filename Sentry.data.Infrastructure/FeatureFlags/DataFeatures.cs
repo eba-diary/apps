24d66843f5e132b4d3ad3ddf12944c73d39574c5
Jered Gosse <Jered.Gosse@sentry.com>
2020-03-20 16:19:59 -0500

    CLA-1563 - Added featureflag to disable uncompreszip execut action

--- Sentry.data.Infrastructure/FeatureFlags/DataFeatures.cs
+++ Sentry.data.Infrastructure/FeatureFlags/DataFeatures.cs
@@ -10,5 +10,6 @@ namespace Sentry.data.Infrastructure.FeatureFlags
         private static IReadableFeatureRepository configRepo = new Sentry.FeatureFlags.SentryConfig.FeatureRepository();
 
         public IFeatureFlag<bool> Expose_BusinessArea_Pages_CLA_1424 { get; } = new BooleanFeatureFlag("Expose_BusinessArea_Pages_CLA_1424", configRepo);
+        public IFeatureFlag<bool> Remove_Mock_Uncompress_Logic_CLA_759 { get; } = new BooleanFeatureFlag("Remove_Mock_Uncompress_Logic_CLA_759", configRepo);
     }
 }
