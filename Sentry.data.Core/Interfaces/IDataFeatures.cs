bfc8d97473e8d4d44f4900bc55c0207356351756
Jered Gosse <Jered.Gosse@sentry.com>
2020-03-20 16:35:16 -0500

    CLA-1565 - Added featureflag to disable converttoparquet executionaction logic

--- Sentry.data.Core/Interfaces/IDataFeatures.cs
+++ Sentry.data.Core/Interfaces/IDataFeatures.cs
@@ -5,5 +5,6 @@ namespace Sentry.data.Core
     public interface IDataFeatures
     {
         IFeatureFlag<bool> Expose_BusinessArea_Pages_CLA_1424 { get; }
+        IFeatureFlag<bool> Remove_ConvertToParquet_Logic_CLA_747 { get; }
     }
 }
--- Sentry.data.Infrastructure/FeatureFlags/DataFeatures.cs
+++ Sentry.data.Infrastructure/FeatureFlags/DataFeatures.cs
@@ -10,5 +10,6 @@ namespace Sentry.data.Infrastructure.FeatureFlags
         private static IReadableFeatureRepository configRepo = new Sentry.FeatureFlags.SentryConfig.FeatureRepository();
 
         public IFeatureFlag<bool> Expose_BusinessArea_Pages_CLA_1424 { get; } = new BooleanFeatureFlag("Expose_BusinessArea_Pages_CLA_1424", configRepo);
+        public IFeatureFlag<bool> Remove_ConvertToParquet_Logic_CLA_747 { get; } = new BooleanFeatureFlag("Remove_ConvertToParquet_Logic_CLA_747", configRepo);
     }
 }
