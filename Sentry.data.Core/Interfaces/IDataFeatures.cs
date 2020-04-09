2277cdfd3ead4468eed356d754f3fea839953255
Austin Laszewski <072186@sentry.com>
2020-04-09 10:21:59 -0500

    CLA-1587 - remove PL landing page feature flags

--- Sentry.data.Core/Interfaces/IDataFeatures.cs
+++ Sentry.data.Core/Interfaces/IDataFeatures.cs
@@ -4,7 +4,6 @@ namespace Sentry.data.Core
 {
     public interface IDataFeatures
     {
-        IFeatureFlag<bool> Expose_BusinessArea_Pages_CLA_1424 { get; }
         IFeatureFlag<bool> Remove_Mock_Uncompress_Logic_CLA_759 { get; }
         IFeatureFlag<bool> Remove_ConvertToParquet_Logic_CLA_747 { get; }
         IFeatureFlag<bool> Expose_TrainingMaterials_CLA_911 { get; }
