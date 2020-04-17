5c91d76d840ce9625838ec121882a20493c28261
Austin Laszewski <072186@sentry.com>
2020-04-17 16:14:27 -0500

    CLA-1501-add service layer

---
+++ Sentry.data.Core/DomainServices/DaleService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sentry.Core;
+using Sentry.data.Core.GlobalEnums;
+using Sentry.Common.Logging;
+
+
+namespace Sentry.data.Core
+{
+    class DaleService : IDaleService
+    {
+        private readonly UserService _userService;
+        private readonly IEventService _eventService;
+
+
+        public DaleService(UserService userService, IEventService eventService)
+        {
+            _userService = userService;
+            _eventService = eventService;
+        }
+
+        public void GetSearchResults(string search)
+        {
+
+        }
+
+
+
+
+    }
+}
