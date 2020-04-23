ba1d3bd10de80ede6cf804adb1cfc6e99e2a790a
Austin Laszewski <072186@sentry.com>
2020-04-23 16:52:36 -0500

    CLA-1499 - refactor daleSearchProvider

--- Sentry.data.Infrastructure/ServiceImplementations/DaleSearchProvider.cs
+++ Sentry.data.Infrastructure/ServiceImplementations/DaleSearchProvider.cs
@@ -11,71 +11,32 @@ namespace Sentry.data.Infrastructure
 {
     public class DaleSearchProvider : IDaleSearchProvider
     {
-
-
         public List<DaleResultDto> GetSearchResults(DaleSearchDto dto)
         {
-
             List<DaleResultDto> daleResults = new List<DaleResultDto>();
 
-            dto.Criteria = "spam";
-            
+            //make sure incoming criteria is valid, or return empty results
+            if (!IsCriteriaValid(dto))
+            {
+                return daleResults;
+            }
+
             string connectionString = Configuration.Config.GetHostSetting("DaleConnectionString");
-
-            string qSelect  = "SELECT  Server_NME,Database_NME,Table_NME,Column_NME,Column_TYP,Precision_LEN,Scale_LEN,Effective_DTM,Expiration_DTM,LastScan_DTM ";
-            string qFrom    = "FROM Column_v ";
-            string qWhereColumn = (dto.Column) ? "Column_NME " : "Table_NME";
-            string qWhereStatement = "WHERE " + qWhereColumn + " LIKE '%" + dto.Criteria + "%'";
-
-            string q = qSelect + qFrom + qWhereStatement;
-
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                // Create the Command and Parameter objects.
-                SqlCommand command = new SqlCommand(q, connection);
+                SqlCommand command = new SqlCommand(GetQuery(dto), connection);
                 //command.Parameters.AddWithValue("@pricePoint", paramValue);
 
                 try
                 {
                     connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
+                    
                     while (reader.Read())
                     {
-                        DaleResultDto result = new DaleResultDto();
-
-                        result.Server           = (!reader.IsDBNull(0)) ? reader.GetString(0) : String.Empty;
-                        result.Database         = (!reader.IsDBNull(1)) ? reader.GetString(1) : String.Empty;
-                        result.Table            = (!reader.IsDBNull(2)) ? reader.GetString(2) : String.Empty;
-                        result.Column           = (!reader.IsDBNull(3)) ? reader.GetString(3) : String.Empty;
-                        result.ColumnType       = (!reader.IsDBNull(4)) ? reader.GetString(4) : String.Empty;
-
-                        if (!reader.IsDBNull(5))
-                        {
-                            result.PrecisionLength = reader.GetInt32(5);
-                        }
-
-                        if (!reader.IsDBNull(6))
-                        {
-                            result.ScaleLength = reader.GetInt32(6);
-                        }
-
-                        if (!reader.IsDBNull(7))
-                        {
-                            result.EffectiveDate = reader.GetDateTime(7);
-                        }
-
-                        if (!reader.IsDBNull(8))
-                        {
-                            result.ExpirationDate = reader.GetDateTime(8);
-                        }
-
-                        if (!reader.IsDBNull(9))
-                        {
-                            result.LastScanDate = reader.GetDateTime(9);
-                        }
-
-                        daleResults.Add(result);
+                        daleResults.Add(CreateDaleResultDto(reader));
                     }
+
                     reader.Close();
                 }
                 catch (Exception ex)
@@ -87,9 +48,66 @@ namespace Sentry.data.Infrastructure
             return daleResults;
         }
 
+        private string GetQuery(DaleSearchDto dto)
+        {
+            string qSelect = "SELECT  Server_NME,Database_NME,Table_NME,Column_NME,Column_TYP,Precision_LEN,Scale_LEN,Effective_DTM,Expiration_DTM,LastScan_DTM ";
+            string qFrom = "FROM Column_v ";
+            string qWhereColumn = (dto.Column) ? "Column_NME " : "Table_NME";
+            string qWhereStatement = "WHERE " + qWhereColumn + " LIKE '%" + dto.Criteria + "%'";
 
+            string q = qSelect + qFrom + qWhereStatement;
 
+            return q;
+        }
 
+        private bool IsCriteriaValid(DaleSearchDto dto)
+        {
+            if (String.IsNullOrWhiteSpace(dto.Criteria) || String.IsNullOrEmpty(dto.Criteria))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
+        private DaleResultDto CreateDaleResultDto(SqlDataReader reader)
+        {
+            DaleResultDto result = new DaleResultDto();
+            result.Server = (!reader.IsDBNull(0)) ? reader.GetString(0) : String.Empty;
+            result.Database = (!reader.IsDBNull(1)) ? reader.GetString(1) : String.Empty;
+            result.Table = (!reader.IsDBNull(2)) ? reader.GetString(2) : String.Empty;
+            result.Column = (!reader.IsDBNull(3)) ? reader.GetString(3) : String.Empty;
+            result.ColumnType = (!reader.IsDBNull(4)) ? reader.GetString(4) : String.Empty;
+
+            if (!reader.IsDBNull(5))
+            {
+                result.PrecisionLength = reader.GetInt32(5);
+            }
+
+            if (!reader.IsDBNull(6))
+            {
+                result.ScaleLength = reader.GetInt32(6);
+            }
+
+            if (!reader.IsDBNull(7))
+            {
+                result.EffectiveDate = reader.GetDateTime(7);
+            }
+
+            if (!reader.IsDBNull(8))
+            {
+                result.ExpirationDate = reader.GetDateTime(8);
+            }
+
+            if (!reader.IsDBNull(9))
+            {
+                result.LastScanDate = reader.GetDateTime(9);
+            }
+
+            return result;
+
+        }
     }
 }
