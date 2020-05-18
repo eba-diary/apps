58f6555e5146d27fbc9848c32a6b25e2912f3352
Gosse Jered <JERED.GOSSE@SENTRY.COM>
2020-05-18 15:14:51 -0500

    CLA-1706 - Exposed VARCHAR length via API

--- Sentry.data.Web/Extensions/SchemaExtensions.cs
+++ Sentry.data.Web/Extensions/SchemaExtensions.cs
@@ -66,7 +66,8 @@ namespace Sentry.data.Web
                 OrdinalPosition = dto.OrdinalPosition,
                 Precision = dto.Precision,
                 Scale = dto.Scale,
-                SourceFormat = (dto.FieldType.ToUpper() == Core.SchemaDatatypes.TIMESTAMP.ToString() || dto.FieldType.ToUpper() == Core.SchemaDatatypes.DATE.ToString()) ? SetFieldDefaults(dto.FieldType, dto.SourceFormat) : dto.SourceFormat
+                SourceFormat = (dto.FieldType.ToUpper() == Core.SchemaDatatypes.TIMESTAMP.ToString() || dto.FieldType.ToUpper() == Core.SchemaDatatypes.DATE.ToString()) ? SetFieldDefaults(dto.FieldType, dto.SourceFormat) : dto.SourceFormat,
+                Length = dto.Length
             };
 
             if (dto.ChildFields.Any())
