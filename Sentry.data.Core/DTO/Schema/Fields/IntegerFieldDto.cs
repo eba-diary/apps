49912e7bfb25c467194a537de3209275af8dab8d
Gosse Jered <JERED.GOSSE@SENTRY.COM>
2020-06-23 09:30:48 -0500

    CLA-1872 - sonar fixes for IntegerFieldDto

--- Sentry.data.Core/DTO/Schema/Fields/IntegerFieldDto.cs
+++ Sentry.data.Core/DTO/Schema/Fields/IntegerFieldDto.cs
@@ -5,12 +5,6 @@ namespace Sentry.data.Core.DTO.Schema.Fields
 {
     public class IntegerFieldDto : BaseFieldDto
     {
-        private int _scale;
-        private int _precision;
-        private string _sourceFormat;
-        private int _ordinalPosition;
-        private int _length;
-
         public IntegerFieldDto(KeyValuePair<string, JsonSchemaProperty> prop, bool array) : base(prop, array) { }
 
         public IntegerFieldDto(IntegerField field) : base(field) { }
@@ -22,11 +16,11 @@ namespace Sentry.data.Core.DTO.Schema.Fields
 
 
         //Properties that are not utilized by this type and are defaulted
-        public override int Precision { get => _precision; set => _precision = value; }
-        public override int Scale { get => _scale; set => _scale = value; }
-        public override string SourceFormat { get => _sourceFormat; set => _sourceFormat = value; }
-        public override int OrdinalPosition { get => _ordinalPosition; set => _ordinalPosition = value; }
-        public override int Length { get => _length; set => _length = value; }
+        public override int Precision { get; set; }
+        public override int Scale { get; set; }
+        public override string SourceFormat { get; set; }
+        public override int OrdinalPosition { get; set; }
+        public override int Length { get; set; }
 
         public override bool CompareToEntity(BaseField field)
         {
