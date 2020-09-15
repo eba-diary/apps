f7ab2da1e05523693290868b2b02a7d78a09972b
Austin Laszewski <072186@sentry.com>
2020-09-15 07:56:27 -0500

    CLA-2227 - working endpoint, figured id check in before getting serious.

---
+++ Sentry.data.Web/Controllers/WebApi/DataInventoryController.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NJsonSchema;
+using Sentry.Common.Logging;
+using Sentry.Core;
+using Sentry.data.Common;
+using Sentry.data.Core;
+using Sentry.data.Core.Exceptions;
+using Sentry.data.Core.Factories.Fields;
+using Sentry.data.Web.Models.ApiModels.Dataset;
+using Sentry.data.Web.Models.ApiModels.Schema;
+using Sentry.WebAPI.Versioning;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Sentry.data.Web.WebApi.Controllers
+{
+    [RoutePrefix(WebConstants.Routes.VERSION_METADATA)]
+    //Users need aleast UseApp permission to access any endpoint on this controller
+    [WebApiAuthorizeUseApp]
+    public class DataInventoryController : BaseWebApiController
+    {
+
+       
+        [HttpGet]
+        [ApiVersionBegin(Sentry.data.Web.WebAPI.Version.v2)]
+        [Route("DoesItContainSensitive")]
+        [SwaggerResponse(System.Net.HttpStatusCode.OK, null, typeof(bool))]
+        [SwaggerResponse(System.Net.HttpStatusCode.InternalServerError, null, null)]
+        public async Task<IHttpActionResult> DoesItContainSensitive()
+        {
+            return Ok(true);
+        }
+
+
+
+
+    }
+}
