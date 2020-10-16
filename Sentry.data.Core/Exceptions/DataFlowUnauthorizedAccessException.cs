b2d63b2285e880b287f5a029752033d9d894d0c0
Gosse Jered <JERED.GOSSE@SENTRY.COM>
2020-10-16 08:29:13 -0500

    CLA-2195 - Implemented permission changes to DataFlowController, added new dataflow exception, added unit tests for CanManageSchema

---
+++ Sentry.data.Core/Exceptions/DataFlowUnauthorizedAccessException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Sentry.data.Core.Exceptions
+{
+    [Serializable]
+    public class DataFlowUnauthorizedAccessException : UnauthorizedAccessException
+    {
+        public DataFlowUnauthorizedAccessException() { }
+        public DataFlowUnauthorizedAccessException(string message) : base(message) { }
+        public DataFlowUnauthorizedAccessException(string message, Exception inner) : base(message, inner) { }
+        protected DataFlowUnauthorizedAccessException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
