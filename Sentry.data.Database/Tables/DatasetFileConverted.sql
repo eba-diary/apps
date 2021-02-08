2391480f8b731e71979bb5411c0b9aca724a4987
072186 <austin.laszewski@sentry.com>
2021-02-08 09:46:44 -0600

    CLA-2653-add new table DatasetFileConverted for Darko

---
+++ Sentry.data.Database/Tables/DatasetFileConverted.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[DatasetFileConverted]
+(
+	[DatasetFileConvertedID] [int] IDENTITY(1,1) NOT NULL,
+	[FileNME] [varchar](250) NULL,
+	[S3FileBucket] [varchar](250) NULL,
+	[S3FileKey] [varchar](1000) NULL,
+	[S3VersionID] [nvarchar](250) NULL,
+	[S3FileSizeAMT] [bigint] NULL,
+	[S3FileMetadata] [nvarchar](max) NULL,
+	[DatasetID] [int] NULL,
+	[SchemaID] [int] NULL,
+	[SchemaRevisionID] [int] NULL,
+	[FlowExecutionGUID] [varchar](100) NULL,
+	[RunInstanceGUID] [varchar](100) NULL,
+	[CreateDTM] [datetime] NULL
+)
