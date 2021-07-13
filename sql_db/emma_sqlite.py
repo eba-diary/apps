62609cf8cb40bf68fe0b63249ff781845ca3f321
JamesMTucker <j.tucker@mail.utoronto.ca>
2021-07-13 08:39:05 -0400

    Fixed Bug in Author Table. Adding DB diagram

--- sql_db/emma_sqlite.py
+++ sql_db/emma_sqlite.py
@@ -8,25 +8,21 @@ INDEX = {} #create a Python dictionary for the indices
 
 ## TABLES ##
 
-### author - Our Author Table will record the author or believed author of a journal entry. We will use author_id as a Foreign Key from the Journal.
+### author - Our Author Table will record the author or believed author of a journal entry. We will use author_id as a Foreign Key from the diary.author_id.
 
 TABLES['author'] = ("""CREATE TABLE IF NOT EXISTS author (
                                     id INT PRIMARY KEY NOT NULL, 
                                     author_id INT NOT NULL,
                                     given_name TEXT NOT NULL,
                                     middle_name TEXT NULL,
-                                    family_name TEXT NOT NULL,
-                                    CONSTRAINT fk_author
-                                        FOREIGN KEY (author_id)
-                                        REFERENCES diary(author_id)
-                                        ON DELETE CASCADE
+                                    family_name TEXT NOT NULL
                                     );""")
 
 INDEX['author_author_id'] = (
     "CREATE INDEX IF NOT EXISTS author_author_id ON author (author_id);"
 )
 
-# diary - an author writes a diary. We will use `diary` to hold meta-data about the diary.
+# diary - an author writes a diary. We will use a diary table to hold meta-data about the diary. Diaries have entries so the foreign key entry.entry_id references entries.
 
 TABLES['diary'] = ("""CREATE TABLE IF NOT EXISTS diary (
                                     id INT PRIMARY KEY NOT NULL,
@@ -38,8 +34,8 @@ TABLES['diary'] = ("""CREATE TABLE IF NOT EXISTS diary (
                                     date_first INT NULL,
                                     date_last INT NULL,
                                     CONSTRAINT fk_diary
-                                        FOREIGN KEY (entry_id)
-                                        REFERENCES diary(entry_id)
+                                        FOREIGN KEY (author_id)
+                                        REFERENCES author (author_id)
                                         ON DELETE CASCADE 
                             );""")
 
@@ -54,8 +50,14 @@ TABLES['diary_entry'] = ("""CREATE TABLE IF NOT EXISTS diary_entry (
                                     id INT PRIMARY KEY NOT NULL,
                                     entry_id INT NOT NULL,
                                     entry_txt TEXT NOT NULL,
-                                    entry_tei TEXT NOT NULL
+                                    entry_tei TEXT NOT NULL,
+                                    entry_setiment INT,
+                                    CONSTRAINT fk_diary_entry
+                                        FOREIGN KEY (entry_id)
+                                        REFERENCES diary (entry_id)
+                                        ON DELETE CASCADE
                             );""")
+
 
 
 ## Create Database
