6b85fcd29ef9fff1a920f9d1dd58bbafb384af52
072186 <austin.laszewski@sentry.com>
2021-07-27 16:30:37 -0500

    CLA-2999-add new post deployment script to remove dataelement tables

---
+++ Sentry.data.Database/Scripts/Post-Deploy/SupportingScripts/Sprint_21_03_06/DELETE_DATA_ELEMENT_TABLES.sql
@@ -0,0 +1,13 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+	07.27.2021		ALL		Delete Deprecated DATA ELEMENT TABLES because DACPAC doesn't remove tables that do not exist in DACPAC
+--------------------------------------------------------------------------------------
+*/
+
+DROP TABLE DataElementDetail
+DROP TABLE DataElement
+DROP TABLE DataObjectDetail
+DROP TABLE DataObjectFieldDetail
+DROP TABLE DataObjectField
+DROP TABLE DataObject
