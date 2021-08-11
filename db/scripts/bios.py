6fce8497c558066175f69277abc6b973988445ff
JamesMTucker <j.tucker@mail.utoronto.ca>
2021-08-11 08:50:00 -0400

    processed master index

--- db/scripts/bios.py
+++ db/scripts/bios.py
@@ -1,6 +1,7 @@
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+import sqlite3
 
 scopes = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('../data/biographies.json', scopes)
@@ -14,4 +15,28 @@ records_data = sheet_persName.get_all_records()
 
 # Convert to DF and process into Database
 # persName=@REF TAG TEI, biography=XML, occupation=OCCUPATION, birth_place=BIRTHPLACE, death_place=PLACE OF DEATH, birth=DATE OF BIRTH, death=DATE OF DEATH
-bios_df = pd.DataFrame.from_dict(records_data)
+bios = pd.DataFrame.from_dict(records_data)
+bios.drop(["Complete SK",
+            "Alt_Name",
+            "Variants",
+            "AUTHORITY_FILE",
+            "RESEARCH",
+            "VOLUME(S)",
+            "BIO ON EBA WEBSITE (Y/N)",
+            "RESEARCH FOLDER IN GOOGLE DRIVE",
+            "IMAGE SOURCE",
+            "IMAGE",
+            "REFERENCE RESOURCES USED (eg books, websites etc)",
+            "OCCUPATION",
+            "BIOGRAPHICAL NOTES",
+            "STATIC IMAGE URL",
+            "INTERN INITIALS"], axis=1, inplace=True)
+
+DB_NAME = "EBA.db"
+with sqlite3.connect(DB_NAME) as db:
+    cur = db.cursor()
+    bioIdx = 1
+    for row in bios.iterrows():
+        persName, biography, birth_place, death_place, birth, death = row[1][1], str(row[1][6]), str(row[1][3]), str(row[1][5]), str(row[1][2]), str(row[1][4])
+        cur.execute("INSERT INTO biography (id, persName, person_id, biography, birth_place, death_place, birth, death) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (bioIdx, persName, bioIdx, biography, birth_place, death_place, birth, death))
+        bioIdx += 1
