199ee6880f33c2e14b4a341f34293aad2a0e0061
Jered Gosse <jered.gosse@sentry.com>
2021-08-24 06:34:19 -0500

    CLA-2888 - updated retrieverjob objectstatus initialize script

--- Sentry.data.Database/Scripts/Post-Deploy/SupportingScripts/Sprint_21_04_02/Initialize_RetrieverJob_ObjectStatus_Columns.sql
+++ Sentry.data.Database/Scripts/Post-Deploy/SupportingScripts/Sprint_21_04_02/Initialize_RetrieverJob_ObjectStatus_Columns.sql
@@ -6,18 +6,21 @@ from RetrieverJob
 
 IF COL_LENGTH('dbo.RetrieverJob', 'ObjectStatus') IS NULL
 BEGIN
+	Print 'Did not detect ObjectStatus on RetrieverJob'
     Alter table #NewRetrieverJob
 	Add ObjectStatus INT NULL
 END
 
 IF COL_LENGTH('dbo.RetrieverJob', 'DeleteIssuer') IS NULL
 BEGIN
+	Print 'Did not detect ObjectStatus on DeleteIssuer'
     Alter table #NewRetrieverJob
 	Add DeleteIssuer VARCHAR(10) NULL
 END
 
 IF COL_LENGTH('dbo.RetrieverJob', 'DeleteIssueDTM') IS NULL
 BEGIN
+	Print 'Did not detect ObjectStatus on DeleteIssueDTM'
     Alter table #NewRetrieverJob
 	Add DeleteIssueDTM datetime NULL
 END
@@ -25,7 +28,8 @@ END
 
 /* All retriever jobs associated with dataflow */
 IF OBJECT_ID('tempdb..#JobsAssoicatedWithDataflows') IS NOT NULL DROP TABLE #JobsAssoicatedWithDataflows
-select Job_Id, DataFlow_ID into #JobsAssoicatedWithDataflows from #NewRetrieverJob where DataFlow_ID is not null
+GO
+select Job_id as 'Identified_Job_Id', DataFlow_ID into #JobsAssoicatedWithDataflows from #NewRetrieverJob where DataFlow_ID is not null
 
 
 /****************************************************************************
@@ -33,12 +37,13 @@ select Job_Id, DataFlow_ID into #JobsAssoicatedWithDataflows from #NewRetrieverJ
 	Update identified retrieverjob records with appropriate objectstatus metadata
 ****************************************************************************/
 IF OBJECT_ID('tempdb..#IdentifiedJobs_NonACTIVE') IS NOT NULL DROP TABLE #IdentifiedJobs_NonACTIVE
+GO
 select
 DF.Id,
 DF.DeleteIssueDTM as 'DF_DeleteIssueDTM',
 DF.DeleteIssuer as 'DF_DeleteIssuer',
 DF_ObjStat.ObjectStatus_CDE as 'DF_ObjStat',
-Jobs.Job_ID as 'J_ID',
+Jobs.Identified_Job_Id as 'J_ID',
 RJ.ObjectStatus as 'RJ_ObjStat',
 RJ.IsEnabled,
 DSrc.SourceType_IND,
@@ -50,7 +55,7 @@ join #JobsAssoicatedWithDataflows Jobs on
 left join ObjectStatus DF_ObjStat on
 	DF.ObjectStatus = DF_ObjStat.ObjectStatus_Id
 left join #NewRetrieverJob RJ on
-	Jobs.Job_ID = RJ.Job_ID
+	Jobs.Identified_Job_Id = RJ.Job_ID
 left join DataSource DSrc on
 	RJ.DataSource_ID = DSrc.DataSource_Id
 where DF_ObjStat.ObjectStatus_CDE <> 'ACTIVE' 
@@ -60,7 +65,7 @@ where DF_ObjStat.ObjectStatus_CDE <> 'ACTIVE'
 Update #NewRetrieverJob
 	SET ObjectStatus = Job_New_ObjStatus, DeleteIssueDTM = DF_DeleteIssueDTM, DeleteIssuer = DF_DeleteIssuer, IsEnabled = 0, Modified_DTM = GETDATE()
 from #IdentifiedJobs_NonACTIVE x
-where Job_id = x.J_ID
+where Job_ID = x.J_ID
 
 
 /****************************************************************************
@@ -68,12 +73,13 @@ where Job_id = x.J_ID
 	Update identified retrieverjob records with appropriate objectstatus metadata
 ****************************************************************************/
 IF OBJECT_ID('tempdb..#IdentifiedDFJobs_ACTIVE') IS NOT NULL DROP TABLE #IdentifiedDFJobs_ACTIVE
+GO
 select
 DF.Id,
 DF.DeleteIssueDTM as 'DF_DeleteIssueDTM',
 DF.DeleteIssuer as 'DF_DeleteIssuer',
 DF_ObjStat.ObjectStatus_CDE as 'DF_ObjStat',
-Jobs.Job_ID as 'J_ID',
+Jobs.Identified_Job_Id as 'J_ID',
 RJ.ObjectStatus as 'RJ_ObjStat',
 RJ.IsEnabled,
 DSrc.SourceType_IND,
@@ -85,7 +91,7 @@ join #JobsAssoicatedWithDataflows Jobs on
 left join ObjectStatus DF_ObjStat on
 	DF.ObjectStatus = DF_ObjStat.ObjectStatus_Id
 left join #NewRetrieverJob RJ on
-	Jobs.Job_ID = RJ.Job_ID
+	Jobs.Identified_Job_Id = RJ.Job_ID
 left join DataSource DSrc on
 	RJ.DataSource_ID = DSrc.DataSource_Id
 where DF_ObjStat.ObjectStatus_CDE = 'ACTIVE' and RJ.ObjectStatus is null and RJ.IsEnabled = 1
@@ -105,6 +111,7 @@ where Job_ID = x.J_ID
 ****************************************************************************/
 
 IF OBJECT_ID('tempdb..#JobsAssoicatedWithSchema') IS NOT NULL DROP TABLE #JobsAssoicatedWithSchema
+GO
 select 
 DFC.Config_ID,
 DFC.Schema_Id,
@@ -147,8 +154,9 @@ where Job_Id = x.J_ID
 ****************************************************************************/
 
 IF OBJECT_ID('tempdb..#legacyprocessingplatformjobs') IS NOT NULL DROP TABLE #legacyprocessingplatformjobs
+GO
 select 
-RJ.Job_Id as 'J_ID',
+RJ.Job_ID as 'J_ID',
 DSrc.SourceType_IND,
 RJ.IsEnabled,
 RJ.Modified_DTM,
@@ -172,7 +180,7 @@ SET Modified_DTM = GETDATE(),
 	DeleteIssueDTM = GETDATE(), 
 	DeleteIssuer = '072984'
 from #legacyprocessingplatformjobs x
-where Job_id = x.J_ID
+where Job_ID = x.J_ID
 
 
 /****************************************************************************
@@ -181,8 +189,9 @@ where Job_id = x.J_ID
 ****************************************************************************/
 
 IF OBJECT_ID('tempdb..#Legacy_nonbasic_external_retrieverjobs') IS NOT NULL DROP TABLE #Legacy_nonbasic_external_retrieverjobs
+GO
 Select 
-RJ.Job_Id as 'J_ID',
+RJ.Job_ID as 'J_ID',
 DSrc.SourceType_IND,
 RJ.IsEnabled,
 RJ.Modified_DTM,
@@ -204,7 +213,7 @@ SET	Modified_DTM = GETDATE(),
 	DeleteIssueDTM = GETDATE(), 
 	DeleteIssuer = '072984'
 from #Legacy_nonbasic_external_retrieverjobs x
-where Job_id = x.J_ID
+where Job_ID = x.J_ID
 
 
 /****************************************************************************
@@ -245,10 +254,20 @@ END
 ****************************************************************************/
 If (@ExecuteScript = 'TRUE')
 BEGIN
+
+	Select
+	Job_Id as 'J_Id',
+	ObjectStatus as 'new_Ostat',
+	DeleteIssuer as 'new_deleteIssuer',
+	DeleteIssueDTM as 'new_deleteIssueDTM',
+	Modified_DTM as 'new_ModifiedDTM'
+	into #Updated_Records
+	from #NewRetrieverJob
+
 	Update RetrieverJob
-	SET ObjectStatus = x.ObjectStatus, DeleteIssuer = x.DeleteIssuer, DeleteIssueDTM = x.DeleteIssueDTM, ModifiedDTM = x.Modified_DTM
-	from #NewRetrieverJob x
-	where Job_Id = x.Job_ID
+	SET ObjectStatus = x.new_Ostat, DeleteIssuer = x.new_deleteIssuer, DeleteIssueDTM = x.new_deleteIssueDTM, Modified_DTM = x.new_ModifiedDTM
+	from #Updated_Records x
+	where Job_ID = x.J_Id	
 END
 else
 BEGIN
