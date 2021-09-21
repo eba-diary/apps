1c9cb91e6f0d79b09f2b3b4e903971ee37e1b158
Jered Gosse <072984@sentry.com>
2021-09-21 10:53:19 -0500

    Move predeployment script logic into static merge scripts

--- Sentry.data.Database/Scripts/Pre-Deploy/_PreDeployment.sql
+++ Sentry.data.Database/Scripts/Pre-Deploy/_PreDeployment.sql
@@ -1,4 +1,5 @@
-﻿DECLARE @ScriptVersion AS VARCHAR(50) 
+﻿/*
+DECLARE @ScriptVersion AS VARCHAR(50) 
 SET @ScriptVersion = '2021.09.22_01_PreDeploy'
 
 BEGIN TRAN 
@@ -7,10 +8,7 @@ IF NOT EXISTS (SELECT * FROM [Version] where Version_CDE=@ScriptVersion)
 BEGIN TRY 
 
   --insert one off script files here
-  :r ..\Pre-Deploy\SupportingScripts\Sprint_21_04_04\Add_Flag__CLA1656_DataFlowEdit_SubmitEditPage.sql
-  :r ..\Pre-Deploy\SupportingScripts\Sprint_21_04_04\Add_Flag__CLA1656_DataFlowEdit_ViewEditPage.sql
-  --insert one off script files here
-  :r ..\Pre-Deploy\SupportingScripts\Sprint_21_09_08\INSERT_SECURITY_DSC_BUSINESS_AREA.sql
+  --:r ..\Pre-Deploy\SupportingScripts\Sprint_21_04_04\Add_Flag__CLA1656_DataFlowEdit_SubmitEditPage.sql
   
   --insert into the verision table so these scripts do not run again.
   INSERT INTO VERSION (Version_CDE, AppliedOn_DTM) VALUES ( @ScriptVersion, GETDATE() ) 
@@ -37,3 +35,4 @@ BEGIN CATCH
 END CATCH 
   
 COMMIT TRAN
+*/
