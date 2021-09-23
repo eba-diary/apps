bbfcc03bf4b2a1d0ea29a955ace0a82922cb24c9
Jered Gosse <072984@sentry.com>
2021-09-23 11:58:50 -0500

    CLA-3255 - adding nhibernate mappin

--- Sentry.data.Infrastructure/Mappings/Primary/Retriever/SubmissionMapping.cs
+++ Sentry.data.Infrastructure/Mappings/Primary/Retriever/SubmissionMapping.cs
@@ -41,6 +41,9 @@ namespace Sentry.data.Infrastructure.Mappings.Primary
                 m.NotNullable(true);
             });
 
+            Property(x => x.FlowExecutionGuid, m => { m.Column("FlowExecutionGuid"); });
+            Property(x => x.RunInstanceGuid, m => { m.Column("RunInstanceGuid"); });
+
             this.ManyToOne(x => x.JobId, m =>
             {
                 m.Column("Job_ID");
