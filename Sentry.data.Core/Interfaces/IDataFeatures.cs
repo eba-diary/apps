99f8a24d41f98ec70b2a6170351a5d402fe0b62a
Jered Gosse <jered.gosse@sentry.com>
2021-10-08 10:10:24 -0500

    CLA-3497 - Added random character string to Livy session name, Added CLA3497_UniqueLivySessionName feature flag

--- Sentry.data.Core/Interfaces/IDataFeatures.cs
+++ Sentry.data.Core/Interfaces/IDataFeatures.cs
@@ -19,6 +19,6 @@ namespace Sentry.data.Core
         IFeatureFlag<bool> CLA3241_DisableDfsDropLocation { get; }
         IFeatureFlag<bool> CLA3332_ConsolidatedDataFlows { get; }
         IFeatureFlag<bool> CLA3048_StandardizeOnUTCTime { get; }
-
+        IFeatureFlag<bool> CLA3497_UniqueLivySessionName { get; }
     }
 }
