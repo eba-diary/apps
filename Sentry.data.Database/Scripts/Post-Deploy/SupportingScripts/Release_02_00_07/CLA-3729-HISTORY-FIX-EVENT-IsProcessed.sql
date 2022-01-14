6bde46c11b1c0a7259c8b31a20c2f9f59b5131a9
072186 <austin.laszewski@sentry.com>
2022-01-14 13:58:03 -0600

    CLA-3749-history fix addition and appconfig change

---
+++ Sentry.data.Database/Scripts/Post-Deploy/SupportingScripts/Release_02_00_07/CLA-3729-HISTORY-FIX-EVENT-IsProcessed.sql
@@ -0,0 +1,49 @@
+﻿SET @ScriptVersion = 'CLA-3729-HISTORY-FIX-EVENT-IsProcessed'
+
+BEGIN TRAN 
+IF NOT EXISTS (SELECT * FROM [Version] where Version_CDE=@ScriptVersion) 
+BEGIN TRY 
+    IF @ScriptVersion LIKE '***%' THROW 100001, 'Error running post-deploy script: the ScriptVersion was still set to the default value!', 1
+    PRINT 'Running script "' + @ScriptVersion + '"...'
+    -- BEGIN POST-DEPLOY SCRIPT --
+
+
+
+
+
+	/**************************************************************************************************************************************
+	This History fix will update Event.IsProcessed for all of history so on Day 1
+	select * from [Event]
+	***************************************************************************************************************************************/
+	--MARK ALL HISTORY IsProcessed = 1
+	UPDATE E
+	SET E.IsProcessed = 1
+	--SELECT E.Reason,E.IsProcessed,ET.Display_IND
+	FROM	[Event] E
+	JOIN	EventType ET 
+				ON E.EventType = ET.[Type_ID]
+	WHERE ET.Description IN ( 'Created Dataset','Created Dataset Schema','Created Report')
+			AND IsProcessed = 0
+
+			
+
+
+
+
+    -- END POST-DEPLOY SCRIPT --
+    INSERT INTO VERSION (Version_CDE, AppliedOn_DTM) VALUES ( @ScriptVersion, GETDATE() ) 
+END TRY 
+BEGIN CATCH 
+    SELECT 
+        @ErrorMessage = ERROR_MESSAGE(), 
+        @ErrorSeverity = ERROR_SEVERITY(), 
+        @ErrorState = ERROR_STATE(); 
+  
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState ); 
+  
+    ROLLBACK TRAN 
+    RETURN
+END CATCH 
+
+COMMIT TRAN
+
--- Sentry.data.Database/Scripts/Post-Deploy/_PostDeployment.sql
+++ Sentry.data.Database/Scripts/Post-Deploy/_PostDeployment.sql
@@ -76,4 +76,5 @@ DECLARE @ErrorState INT;
 :r ..\Post-Deploy\SupportingScripts\Release_02_00_03\CLA-3465-HISTORY-FIX-SCHEMA-SnowflakeWarehouse.sql
 :r ..\Post-Deploy\SupportingScripts\Release_02_00_03\CLA3606_Move_CLA3240_UseDropLocationV2_to_LaunchDarkly.sql
 :r ..\Post-Deploy\SupportingScripts\Release_02_00_07\CLA-3729-HISTORY-FIX-EVENT-Reason.sql
+:r ..\Post-Deploy\SupportingScripts\Release_02_00_07\CLA-3729-HISTORY-FIX-EVENT-IsProcessed.sql
 
