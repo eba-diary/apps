8e978de2c404fa03a7707cc031da2d72f5dc12cd
Caden Untiedt <082116@sentry.com>
2022-02-04 13:36:09 -0600

    Add info icon and warning toast for no sensitive permissions

--- Sentry.data.Web/js/DataInventory.js
+++ Sentry.data.Web/js/DataInventory.js
@@ -1,7 +1,7 @@
 ﻿data.DataInventory = {
 
     init: function () {
-        this.initDataTable();
+        this.initDataInventory();
         this.buildFilter();
         this.initEvents();
     },
@@ -17,76 +17,104 @@
         $.post("/DataInventory/SearchFilters/", data.DataInventory.buildRequest(), (x) => data.FilterSearch.completeFilterRetrieval(x));
     },
 
-    initDataTable: function () {
+    initDataInventory: function () {
         $.ajax({
             url: "/DataInventory/GetCanDaleSensitive/",
             method: "GET",
             dataType: 'json',
             success: function (obj) {
-                $("#di-result-table").DataTable({
-                    pageLength: 20,
-                    deferRender: true,
-                    ordering: false,
-                    ajax: {
-                        url: "/DataInventory/SearchResult/",
-                        type: "POST",
-                        data: data.DataInventory.buildRequest
-                    },
-                    columns: [
-                        {
-                            data: null, className: "Asset", searchable: true, render: function (data) {
-                                //the following render func is called for every single row column and passes in data as the specific value.  our func body below will insert our list of assets as links
-                                if (data.AssetList != null) {
+                data.DataInventory.initDataTable(obj);
 
-                                    var len = data.AssetList.length;
-                                    var assetHtml = '';
+                if (!obj.canDaleSensitiveView) {
+                    toastr.options = {
+                        "closeButton": false,
+                        "debug": false,
+                        "newestOnTop": false,
+                        "progressBar": false,
+                        "positionClass": "toast-top-right",
+                        "preventDuplicates": false,
+                        "onclick": null,
+                        "showDuration": "1000",
+                        "hideDuration": "1000",
+                        "timeOut": "8000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                    };
 
-                                    if (len > 0) {
-                                        for (let i = 0; i < len; i++) {
+                    toastr['error']("All results may not be displayed. " +
+                        "Additional permission is needed to view columns marked " +
+                        "as sensitive. Please click the Data Inventory info icon for more information.");
+                }                
+            }
+        });
+    },
 
-                                            var ri = data.AssetList[i];
-                                            var aTag = '<a target="_blank" rel="noopener noreferrer" href=https://said.sentry.com/ViewAsset.aspx?ID=' + ri + '\>' + ri + '</a>';
-                                            assetHtml = assetHtml + ' ' + aTag;
-                                        }
-                                    }
+    initDataTable: function (obj) {
+        $("#di-result-table").DataTable({
+            pageLength: 20,
+            deferRender: true,
+            ordering: false,
+            ajax: {
+                url: "/DataInventory/SearchResult/",
+                type: "POST",
+                data: data.DataInventory.buildRequest
+            },
+            columns: [
+                {
+                    data: null, className: "Asset", searchable: true, render: function (data) {
+                        //the following render func is called for every single row column and passes in data as the specific value.  our func body below will insert our list of assets as links
+                        if (data.AssetList != null) {
+
+                            var len = data.AssetList.length;
+                            var assetHtml = '';
+
+                            if (len > 0) {
+                                for (let i = 0; i < len; i++) {
+
+                                    var ri = data.AssetList[i];
+                                    var aTag = '<a target="_blank" rel="noopener noreferrer" href=https://said.sentry.com/ViewAsset.aspx?ID=' + ri + '\>' + ri + '</a>';
+                                    assetHtml = assetHtml + ' ' + aTag;
                                 }
-                                return assetHtml;
                             }
-                        },
-                        { data: "Server", searchable: true, className: "Server" },
-                        { data: "Database", className: "Database" },
-                        { data: "Object", className: "Object" },
-                        { data: "ObjectType", className: "ObjectType" },
-                        { data: "Column", className: "ColumnMan" },
-                        //ISSENSITIVE CHECKBOX
-                        //the key piece here is including a label with text to indicate whether IsSensitive column is true or false so the filtering works
-                        //Since I did not want user to see label text and still have a filter.  My cheat to this was to style label with display:none while still keeping the filtering ability
-                        //later on when they check/uncheck the box my editRow() function will refresh the data associated with the grid which changes the label hidden text to the opposite so filtering can refresh
-                        { data: null, className: "IsSensitive", visible: false, render: (d) => data.DataInventory.getTableElementCheckbox(!obj.canDaleSensitiveEdit || (obj.canDaleSensitiveEdit && !obj.canDaleOwnerVerifiedEdit && d.IsOwnerVerified) || !obj.CLA3707_UsingSQLSource, d.IsSensitive) },
-                        //OWNER VERIFIED CHECKBOX
-                        { data: null, className: "IsOwnerVerified", visible: false, render: (d) => data.DataInventory.getTableElementCheckbox(!obj.canDaleOwnerVerifiedEdit || !obj.CLA3707_UsingSQLSource, d.IsOwnerVerified) },
-                        { data: "ProdType", className: "ProdType", visible: false },
-                        { data: "ColumnType", className: "ColumnType", visible: false },
-                        { data: "MaxLength", className: "MaxLength", visible: false },
-                        { data: "Precision", className: "Precision", visible: false },
-                        { data: "Scale", className: "Scale", visible: false },
-                        { data: "IsNullable", className: "IsNullable", visible: false },
-                        { data: "EffectiveDate", className: "EffectiveDate", visible: false },
-                        { data: "SourceType", className: "SourceType", visible: false },
-                        { data: "ScanCategory", className: "ScanCategory", visible: false },
-                        { data: "ScanType", className: "ScanType", visible: false }
-                    ],
-                    aLengthMenu: [10, 20, 50, 100, 500],
-                    dom: "<'row'" +
-                        "<'col-xs-6'l>" +
-                        "<'col-xs-6 text-right'B>>" +
-                        "<'row'<'col-xs-12'tr>>" +
-                        "<'row'<'col-xs-12 text-center'p>>",
-                    buttons: [{ extend: 'colvis', text: 'Columns' }],
-                    initComplete: function (settings, json) {
-                        data.FilterSearch.completeSearch(json.searchTotal, settings.oInit.pageLength, json.data.length);
+                        }
+                        return assetHtml;
                     }
-                });
+                },
+                { data: "Server", searchable: true, className: "Server" },
+                { data: "Database", className: "Database" },
+                { data: "Object", className: "Object" },
+                { data: "ObjectType", className: "ObjectType" },
+                { data: "Column", className: "ColumnMan" },
+                //ISSENSITIVE CHECKBOX
+                //the key piece here is including a label with text to indicate whether IsSensitive column is true or false so the filtering works
+                //Since I did not want user to see label text and still have a filter.  My cheat to this was to style label with display:none while still keeping the filtering ability
+                //later on when they check/uncheck the box my editRow() function will refresh the data associated with the grid which changes the label hidden text to the opposite so filtering can refresh
+                { data: null, className: "IsSensitive", visible: false, render: (d) => data.DataInventory.getTableElementCheckbox(!obj.canDaleSensitiveEdit || (obj.canDaleSensitiveEdit && !obj.canDaleOwnerVerifiedEdit && d.IsOwnerVerified) || !obj.CLA3707_UsingSQLSource, d.IsSensitive) },
+                //OWNER VERIFIED CHECKBOX
+                { data: null, className: "IsOwnerVerified", visible: false, render: (d) => data.DataInventory.getTableElementCheckbox(!obj.canDaleOwnerVerifiedEdit || !obj.CLA3707_UsingSQLSource, d.IsOwnerVerified) },
+                { data: "ProdType", className: "ProdType", visible: false },
+                { data: "ColumnType", className: "ColumnType", visible: false },
+                { data: "MaxLength", className: "MaxLength", visible: false },
+                { data: "Precision", className: "Precision", visible: false },
+                { data: "Scale", className: "Scale", visible: false },
+                { data: "IsNullable", className: "IsNullable", visible: false },
+                { data: "EffectiveDate", className: "EffectiveDate", visible: false },
+                { data: "SourceType", className: "SourceType", visible: false },
+                { data: "ScanCategory", className: "ScanCategory", visible: false },
+                { data: "ScanType", className: "ScanType", visible: false }
+            ],
+            aLengthMenu: [10, 20, 50, 100, 500],
+            dom: "<'row'" +
+                "<'col-xs-6'l>" +
+                "<'col-xs-6 text-right'B>>" +
+                "<'row'<'col-xs-12'tr>>" +
+                "<'row'<'col-xs-12 text-center'p>>",
+            buttons: [{ extend: 'colvis', text: 'Columns' }],
+            initComplete: function (settings, json) {
+                data.FilterSearch.completeSearch(json.searchTotal, settings.oInit.pageLength, json.data.length);
             }
         });
     },
