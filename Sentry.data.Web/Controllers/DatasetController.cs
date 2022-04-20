98b3a12c7b35287abe5c8820eaba5c5d937c4b94
Sam Smith <Samuel.Smith@sentry.com>
2022-04-20 11:48:06 -0500

    CLA-3969: Repopulated category names on validation error

--- Sentry.data.Web/Controllers/DatasetController.cs
+++ Sentry.data.Web/Controllers/DatasetController.cs
@@ -293,8 +293,8 @@ namespace Sentry.data.Web.Controllers
             }
 
             Utility.SetupLists(_datasetContext, model);
+            model.CategoryNames = model.AllCategories.Where(cat => model.DatasetCategoryIds.Contains(int.Parse(cat.Value))).Select(cat => cat.Text).ToList();
             model.SAIDAssetDropDown = await BuildSAIDAssetDropDown(model.SAIDAssetKeyCode);
-
             var namedEnvironments = await _namedEnvironmentBuilder.BuildNamedEnvironmentDropDowns(model.SAIDAssetKeyCode, model.NamedEnvironment);
             model.NamedEnvironmentDropDown = namedEnvironments.namedEnvironmentList;
             model.NamedEnvironmentTypeDropDown = namedEnvironments.namedEnvironmentTypeList;
