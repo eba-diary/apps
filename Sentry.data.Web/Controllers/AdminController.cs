b261f9ac57d26ab657a8e8e6c8a2fde04a4b14a6
Ethan Hart <084168@sentry.com>
2022-06-10 10:53:58 -0500

    Admin setup

---
+++ Sentry.data.Web/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Data;
+using Sentry.data.Core;
+using Sentry.DataTables.QueryableAdapter;
+using Sentry.DataTables.Mvc;
+using Sentry.DataTables.Shared;
+using DoddleReport.Web;
+using DoddleReport;
+using Sentry.Core;
+using System.Threading.Tasks;
+
+namespace Sentry.data.Web.Controllers
+{
+    public class AdminController : BaseController
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
