e1432d68811ee49a4ff88011f0e90b1c087acc67
Ethan Hart <084168@sentry.com>
2022-06-17 16:39:24 -0500

    Create Contuent Connector Provide + IKafkaConnectorProvider interface

---
+++ Sentry.data.Core/Interfaces/IKafkaConnectorProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sentry.data.Core
+{
+    public interface IKafkaConnectorProvider
+    {
+        Task<HttpResponseMessage> PostRequestAsync(string resource, HttpContent postContent);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <exception cref="TaskCanceledException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <returns></returns>
+        Task<HttpResponseMessage> GetRequestAsync(string resource);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <exception cref="TaskCanceledException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <returns></returns>
+        Task<HttpResponseMessage> DeleteRequestAsync(string resource);
+    }
+}
---
+++ Sentry.data.Infrastructure/ServiceImplementations/ContuentConnectorProvider.cs
@@ -0,0 +1,68 @@
+﻿using Polly;
+using Polly.Registry;
+using Sentry.data.Core;
+using Sentry.data.Core.Interfaces;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sentry.data.Infrastructure
+{
+    public class ContuentConnectorProvider : IKafkaConnectorProvider
+    {
+        private readonly IAsyncPolicy _asyncProviderPolicy;
+        private readonly IHttpClientProvider _httpClient;
+
+        public ContuentConnectorProvider(IHttpClientProvider httpClientProvider, IPolicyRegistry<string> policyRegistry)
+        {
+            _httpClient = httpClientProvider;
+            _asyncProviderPolicy = policyRegistry.Get<IAsyncPolicy>(PollyPolicyKeys.ApacheLivyProviderAsyncPolicy);
+        }
+
+        public string BaseUrl { get; set; } = "";
+
+        public async Task<HttpResponseMessage> PostRequestAsync(string resource, HttpContent postContent)
+        {
+            var pollyResponse = await _asyncProviderPolicy.ExecuteAsync(async () =>
+            {
+                var x =  await _httpClient.PostAsync(BaseUrl + $"/{resource}", postContent).ConfigureAwait(false);
+
+                return x;
+
+            }).ConfigureAwait(false);
+
+             HttpResponseMessage response = pollyResponse;
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> GetRequestAsync(string resource)
+        {
+            var pollyResponse = await _asyncProviderPolicy.ExecuteAsync(async () =>
+            {
+                var x = await _httpClient.GetAsync(BaseUrl + resource).ConfigureAwait(false);
+
+                return x;
+
+            }).ConfigureAwait(false);
+
+            HttpResponseMessage response = pollyResponse;
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> DeleteRequestAsync(string resource)
+        {
+            var pollyResponse = await _asyncProviderPolicy.ExecuteAsync(async () =>
+            {
+                var x = await _httpClient.DeleteAsync(BaseUrl + resource).ConfigureAwait(false);
+
+                return x;
+
+            }).ConfigureAwait(false);
+
+            HttpResponseMessage response = pollyResponse;
+
+            return response;
+        }
+    }
+}
