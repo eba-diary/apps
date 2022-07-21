f90ae98f4dfe4665fe43b69087c83b82325cad35
Sam Smith <Samuel.Smith@sentry.com>
2022-07-21 09:39:34 -0500

    CLA-4330: Revert change and rename constant

--- Sentry.data.Infrastructure/ServiceImplementations/TicketProvider/CherwellProvider.cs
+++ Sentry.data.Infrastructure/ServiceImplementations/TicketProvider/CherwellProvider.cs
@@ -43,7 +43,7 @@ namespace Sentry.data.Infrastructure
 
                 AddApproversToTicket(newBusPublicObId, model);
 
-                ChangeStatus(newBusPublicObId, GlobalConstants.CherwellChangeStatusNames.APPROVAL, GlobalConstants.CherwellChangeStatusOrder.APPROVAL);
+                ChangeStatus(newBusPublicObId, GlobalConstants.CherwellChangeStatusNames.WAITING_FOR_APPROVAL, GlobalConstants.CherwellChangeStatusOrder.WAITING_FOR_APPROVAL);
 
                 return newBusPublicObId;
             }
@@ -84,7 +84,7 @@ namespace Sentry.data.Infrastructure
             {
                 ticket.TicketStatus = GlobalConstants.HpsmTicketStatus.DENIED;
             }
-            else if (ticketStatus == GlobalConstants.CherwellChangeStatusNames.IMPLEMENTING || ticketStatus == GlobalConstants.CherwellChangeStatusNames.APPROVAL)
+            else if (ticketStatus == GlobalConstants.CherwellChangeStatusNames.IMPLEMENTING)
             {
                 ticket.TicketStatus = GlobalConstants.HpsmTicketStatus.APPROVED;
             }
