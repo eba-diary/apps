24523f85e1fb00e8b0fa8100628deb5e7e90470b
Ethan Hart <084168@sentry.com>
2022-07-29 16:20:54 -0500

    Created database helper to ensure there are no null values

---
+++ Sentry.data.Core/Helpers/DatabaseHelper.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace Sentry.data.Core.Helpers
+{
+    public static class DatabaseHelper
+    {
+        public static int SafeDatabaseInt(object row)
+        {
+            if (row != null && int.TryParse(row.ToString(), out int result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+
+        public static string SafeDatabaseString(object row)
+        {
+            if (row != null)
+            {
+                return row.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public static DateTime SafeDatabaseDate(object row)
+        {
+            if (row != null && DateTime.TryParse(row.ToString(), out DateTime result))
+            {
+                return result;
+            }
+
+            return DateTime.MinValue;
+        }
+    }
+}
--- Sentry.data.Infrastructure/ServiceImplementations/DeadJobProvider.cs
+++ Sentry.data.Infrastructure/ServiceImplementations/DeadJobProvider.cs
@@ -8,6 +8,7 @@ using Sentry.NHibernate;
 using NHibernate;
 using System.Data.SqlClient;
 using System.Data;
+using Sentry.data.Core.Helpers;
 
 namespace Sentry.data.Infrastructure
 {
@@ -39,28 +40,28 @@ namespace Sentry.data.Infrastructure
                 {
                     DeadSparkJob deadSparkJob = new DeadSparkJob();
 
-                    deadSparkJob.SubmissionID = int.Parse(row["Submission_id"].ToString());
-                    deadSparkJob.SubmissionCreated = Convert.ToDateTime(row["sub_Created"]);
-                    deadSparkJob.DatasetName = row["Dataset_NME"].ToString().Trim();
-                    deadSparkJob.SchemaName = row["Schema_NME"].ToString().Trim();
-                    deadSparkJob.SourceBucketName = row["SourceBucketName"].ToString();
-                    deadSparkJob.SourceKey = row["SourceKey"].ToString();
-                    deadSparkJob.TargetKey = row["TargetKey"].ToString();
-                    deadSparkJob.BatchID = int.Parse(row["BatchId"].ToString());
-                    deadSparkJob.State = row["State"].ToString();
-                    deadSparkJob.LivyAppID = row["LivyAppId"].ToString();
-                    deadSparkJob.LivyDriverlogUrl = row["LivyDriverlogUrl"].ToString();
-                    deadSparkJob.LivySparkUiUrl = row["LivySparkUiUrl"].ToString();
-                    deadSparkJob.DayOfMonth = int.Parse(row["Day of Month"].ToString());
-                    deadSparkJob.HourOfDay = int.Parse(row["Hour of Day"].ToString());
-                    deadSparkJob.TriggerKey = row["TriggerKey"].ToString();
-                    deadSparkJob.TriggerBucket = row["TriggerBucket"].ToString();
-                    deadSparkJob.ExecutionGuid = row["ExecutionGuid"].ToString();
-                    deadSparkJob.DatasetID = int.Parse(row["Dataset_ID"].ToString());
-                    deadSparkJob.SchemaID = int.Parse(row["Schema_ID"].ToString());
-                    deadSparkJob.DatasetFileID = int.Parse(row["DatasetFile_ID"].ToString());
-                    deadSparkJob.DataFlowID = int.Parse(row["DataFlow_ID"].ToString());
-                    deadSparkJob.DataFlowStepID = int.Parse(row["DataFlowStep_ID"].ToString());
+                    deadSparkJob.SubmissionID =         DatabaseHelper.SafeDatabaseInt(row["Submission_id"]);
+                    deadSparkJob.BatchID =              DatabaseHelper.SafeDatabaseInt(row["BatchId"]);
+                    deadSparkJob.DayOfMonth =           DatabaseHelper.SafeDatabaseInt(row["Day of Month"]);
+                    deadSparkJob.HourOfDay =            DatabaseHelper.SafeDatabaseInt(row["Hour of Day"]);
+                    deadSparkJob.DatasetID =            DatabaseHelper.SafeDatabaseInt(row["Dataset_ID"]);
+                    deadSparkJob.SchemaID =             DatabaseHelper.SafeDatabaseInt(row["Schema_ID"]);
+                    deadSparkJob.DatasetFileID =        DatabaseHelper.SafeDatabaseInt(row["DatasetFile_ID"]);
+                    deadSparkJob.DataFlowID =           DatabaseHelper.SafeDatabaseInt(row["DataFlow_ID"]);
+                    deadSparkJob.DataFlowStepID =       DatabaseHelper.SafeDatabaseInt(row["DataFlowStep_ID"]);
+                    deadSparkJob.SubmissionCreated =    DatabaseHelper.SafeDatabaseDate(row["sub_Created"]);
+                    deadSparkJob.DatasetName =          DatabaseHelper.SafeDatabaseString(row["Dataset_NME"]);
+                    deadSparkJob.SchemaName =           DatabaseHelper.SafeDatabaseString(row["Schema_NME"]);
+                    deadSparkJob.SourceBucketName =     DatabaseHelper.SafeDatabaseString(row["SourceBucketName"]);
+                    deadSparkJob.SourceKey =            DatabaseHelper.SafeDatabaseString(row["SourceKey"]);
+                    deadSparkJob.TargetKey =            DatabaseHelper.SafeDatabaseString(row["TargetKey"]);
+                    deadSparkJob.State =                DatabaseHelper.SafeDatabaseString(row["State"]);
+                    deadSparkJob.LivyAppID =            DatabaseHelper.SafeDatabaseString(row["LivyAppId"]);
+                    deadSparkJob.LivyDriverlogUrl =     DatabaseHelper.SafeDatabaseString(row["LivyDriverlogUrl"]);
+                    deadSparkJob.LivySparkUiUrl =       DatabaseHelper.SafeDatabaseString(row["LivySparkUiUrl"]);
+                    deadSparkJob.TriggerKey =           DatabaseHelper.SafeDatabaseString(row["TriggerKey"]);
+                    deadSparkJob.TriggerBucket =        DatabaseHelper.SafeDatabaseString(row["TriggerBucket"]);
+                    deadSparkJob.ExecutionGuid =        DatabaseHelper.SafeDatabaseString(row["ExecutionGuid"]);
 
                     deadSparkJobList.Add(deadSparkJob);
                 }
