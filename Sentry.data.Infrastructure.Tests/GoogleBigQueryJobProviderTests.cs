248b4db0691ece77420dc753de5c2987cc9007f0
Caden Untiedt <caden.untiedt@sentry.com>
2022-09-21 17:27:10 -0500

    Finish unit testing big queryjvider

--- Sentry.data.Infrastructure.Tests/GoogleBigQueryJobProviderTests.cs
+++ Sentry.data.Infrastructure.Tests/GoogleBigQueryJobProviderTests.cs
@@ -1,21 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Moq.Protected;
+using Newtonsoft.Json.Linq;
 using Sentry.data.Core;
+using Sentry.data.Core.Entities.DataProcessing;
 using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
-using Moq.Protected;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-using System.IO;
-using NHibernate.Mapping;
-using Sentry.data.Core.Entities.DataProcessing;
-using System.Collections.Generic;
 using static Sentry.data.Core.GlobalConstants;
-using Nest;
 
 namespace Sentry.data.Infrastructure.Tests
 {
@@ -398,13 +395,177 @@ namespace Sentry.data.Infrastructure.Tests
         [TestMethod]
         public void Execute_RetrieverJob_FailInMiddle()
         {
+            MockRepository repository = new MockRepository(MockBehavior.Strict);
 
+            DateTime today = DateTime.Today;
+
+            HTTPSSource httpsSource = new HTTPSSource() { BaseUri = new Uri("https://bigquery.googleapis.com/bigquery/v2/") };
+
+            Mock<IAuthorizationProvider> authorizationProvider = repository.Create<IAuthorizationProvider>();
+            authorizationProvider.Setup(x => x.GetOAuthAccessToken(httpsSource)).Returns("token");
+
+            Mock<HttpMessageHandler> httpMessageHandler = repository.Create<HttpMessageHandler>();
+
+            HttpResponseMessage httpResponseMessageFields = GetResponseMessage(HttpStatusCode.OK, "EventsSchema");
+            HttpResponseMessage httpResponseMessageData = GetResponseMessage(HttpStatusCode.OK, "Data_PageToken");
+
+            string requestDatePartition = today.AddDays(-1).ToString("yyyyMMdd");
+            string fieldsUri = $"https://bigquery.googleapis.com/bigquery/v2/projects/project1/datasets/dataset1/tables/events_{requestDatePartition}";
+            string dataUri = fieldsUri + "/data";
+            string dataUri2 = dataUri + "?pageToken=BFDECMLBQMAQAAASAUIIBAEAAUNAICAFCACSB77777777777757SUAA%3D";
+            string dataLastUri = $"https://bigquery.googleapis.com/bigquery/v2/projects/project1/datasets/dataset1/tables/events_{today:yyyyMMdd}/data";
+
+            httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                            ItExpr.Is<HttpRequestMessage>(x => x.RequestUri.ToString() == fieldsUri),
+                                                                            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(httpResponseMessageFields);
+            httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                            ItExpr.Is<HttpRequestMessage>(x => x.RequestUri.ToString() == dataUri),
+                                                                            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(httpResponseMessageData);
+            httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                            ItExpr.Is<HttpRequestMessage>(x => x.RequestUri.ToString() == dataUri2),
+                                                                            ItExpr.IsAny<CancellationToken>()).Throws<Exception>();
+            httpMessageHandler.Protected().Setup("Dispose", ItExpr.Is<bool>(x => x));
+
+            HttpClient httpClient = new HttpClient(httpMessageHandler.Object);
+
+            Mock<IHttpClientGenerator> httpClientGenerator = repository.Create<IHttpClientGenerator>();
+            httpClientGenerator.Setup(x => x.GenerateHttpClient()).Returns(httpClient);
+
+            Mock<IDatasetContext> datasetContext = repository.Create<IDatasetContext>();
+
+            DataFlow dataFlow = new DataFlow() { Id = 1, SchemaId = 2 };
+            DataFlowStep dataFlowStep = new DataFlowStep()
+            {
+                DataFlow = dataFlow,
+                DataAction_Type_Id = DataActionType.ProducerS3Drop,
+                TriggerBucket = "bucket",
+                TriggerKey = "key/"
+            };
+
+            string datePartition = today.AddDays(-2).ToString("yyyyMMdd");
+
+            RetrieverJob job = new RetrieverJob()
+            {
+                DataSource = httpsSource,
+                RelativeUri = $"projects/project1/datasets/dataset1/tables/events_{datePartition}/data",
+                DataFlow = dataFlow,
+                JobOptions = new RetrieverJobOptions()
+                {
+                    TargetFileName = "testfile"
+                }
+            };
+
+            int saveCount = 0;
+            datasetContext.SetupGet(x => x.DataFlowStep).Returns(new List<DataFlowStep>() { dataFlowStep }.AsQueryable());
+            datasetContext.Setup(x => x.SaveChanges(true)).Callback(() => saveCount++);
+
+            int s3Count = 0;
+            Mock<IS3ServiceProvider> s3ServiceProvider = repository.Create<IS3ServiceProvider>();
+            s3ServiceProvider.Setup(x => x.UploadDataFile(It.IsAny<Stream>(), "bucket", It.Is<string>(s => s.StartsWith($"key/testfile_events_{requestDatePartition}_"))))
+                .Callback(() => s3Count++).Returns("");
+
+            Mock<IGoogleBigQueryService> bigQueryService = repository.Create<IGoogleBigQueryService>();
+            bigQueryService.Setup(x => x.UpdateSchemaFields(2, It.IsAny<JArray>()));
+
+            GoogleBigQueryJobProvider provider = new GoogleBigQueryJobProvider(datasetContext.Object, s3ServiceProvider.Object, authorizationProvider.Object, httpClientGenerator.Object, bigQueryService.Object);
+
+            Assert.ThrowsException<Exception>(() => provider.Execute(job));
+
+            Assert.AreEqual(1, saveCount);
+            Assert.AreEqual(1, s3Count);
+            Assert.IsTrue(httpClient.DefaultRequestHeaders.Contains("Authorization"));
+            Assert.AreEqual("Bearer token", httpClient.DefaultRequestHeaders.GetValues("Authorization").First());
+            Assert.AreEqual($"projects/project1/datasets/dataset1/tables/events_{requestDatePartition}/data", job.RelativeUri);
+            Assert.AreEqual("5", job.ExecutionParameters[ExecutionParameterKeys.GoogleBigQueryApi.LASTINDEX]);
+            Assert.AreEqual("10", job.ExecutionParameters[ExecutionParameterKeys.GoogleBigQueryApi.TOTALROWS]);
+
+            repository.VerifyAll();
         }
 
         [TestMethod]
         public void Execute_RetrieverJob_StartFromFailure()
         {
+            MockRepository repository = new MockRepository(MockBehavior.Strict);
 
+            DateTime today = DateTime.Today;
+
+            HTTPSSource httpsSource = new HTTPSSource() { BaseUri = new Uri("https://bigquery.googleapis.com/bigquery/v2/") };
+
+            Mock<IAuthorizationProvider> authorizationProvider = repository.Create<IAuthorizationProvider>();
+            authorizationProvider.Setup(x => x.GetOAuthAccessToken(httpsSource)).Returns("token");
+
+            Mock<HttpMessageHandler> httpMessageHandler = repository.Create<HttpMessageHandler>();
+
+            HttpResponseMessage httpResponseMessageFields = GetResponseMessage(HttpStatusCode.OK, "EventsSchema");
+            HttpResponseMessage httpResponseMessageData = GetResponseMessage(HttpStatusCode.OK, "Data_PageTokenLast");
+            HttpResponseMessage httpResponseMessageNotFound = GetResponseMessage(HttpStatusCode.NotFound, "NotFound");
+
+            string requestDatePartition = today.AddDays(-1).ToString("yyyyMMdd");
+            string fieldsUri = $"https://bigquery.googleapis.com/bigquery/v2/projects/project1/datasets/dataset1/tables/events_{requestDatePartition}";
+            string dataUri = fieldsUri + "/data?startIndex=5";
+            string dataLastUri = $"https://bigquery.googleapis.com/bigquery/v2/projects/project1/datasets/dataset1/tables/events_{today:yyyyMMdd}/data";
+
+            httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                            ItExpr.Is<HttpRequestMessage>(x => x.RequestUri.ToString() == fieldsUri),
+                                                                            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(httpResponseMessageFields);
+            httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                            ItExpr.Is<HttpRequestMessage>(x => x.RequestUri.ToString() == dataUri),
+                                                                            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(httpResponseMessageData);
+            httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                            ItExpr.Is<HttpRequestMessage>(x => x.RequestUri.ToString() == dataLastUri),
+                                                                            ItExpr.IsAny<CancellationToken>()).ReturnsAsync(httpResponseMessageNotFound);
+            httpMessageHandler.Protected().Setup("Dispose", ItExpr.Is<bool>(x => x));
+
+            HttpClient httpClient = new HttpClient(httpMessageHandler.Object);
+
+            Mock<IHttpClientGenerator> httpClientGenerator = repository.Create<IHttpClientGenerator>();
+            httpClientGenerator.Setup(x => x.GenerateHttpClient()).Returns(httpClient);
+
+            Mock<IDatasetContext> datasetContext = repository.Create<IDatasetContext>();
+
+            DataFlow dataFlow = new DataFlow() { Id = 1, SchemaId = 2 };
+            DataFlowStep dataFlowStep = new DataFlowStep()
+            {
+                DataFlow = dataFlow,
+                DataAction_Type_Id = DataActionType.ProducerS3Drop,
+                TriggerBucket = "bucket",
+                TriggerKey = "key/"
+            };
+
+            RetrieverJob job = new RetrieverJob()
+            {
+                DataSource = httpsSource,
+                RelativeUri = $"projects/project1/datasets/dataset1/tables/events_{requestDatePartition}/data",
+                DataFlow = dataFlow,
+                JobOptions = new RetrieverJobOptions()
+                {
+                    TargetFileName = "testfile"
+                }
+            };
+
+            job.AddOrUpdateExecutionParameter(ExecutionParameterKeys.GoogleBigQueryApi.LASTINDEX, "5");
+            job.AddOrUpdateExecutionParameter(ExecutionParameterKeys.GoogleBigQueryApi.TOTALROWS, "10");
+
+            datasetContext.SetupGet(x => x.DataFlowStep).Returns(new List<DataFlowStep>() { dataFlowStep }.AsQueryable());
+            datasetContext.Setup(x => x.SaveChanges(true));
+
+            Mock<IS3ServiceProvider> s3ServiceProvider = repository.Create<IS3ServiceProvider>();
+            s3ServiceProvider.Setup(x => x.UploadDataFile(It.IsAny<Stream>(), "bucket", It.Is<string>(s => s.StartsWith($"key/testfile_events_{requestDatePartition}_")))).Returns("");
+
+            Mock<IGoogleBigQueryService> bigQueryService = repository.Create<IGoogleBigQueryService>();
+            bigQueryService.Setup(x => x.UpdateSchemaFields(2, It.IsAny<JArray>()));
+
+            GoogleBigQueryJobProvider provider = new GoogleBigQueryJobProvider(datasetContext.Object, s3ServiceProvider.Object, authorizationProvider.Object, httpClientGenerator.Object, bigQueryService.Object);
+
+            provider.Execute(job);
+
+            Assert.IsTrue(httpClient.DefaultRequestHeaders.Contains("Authorization"));
+            Assert.AreEqual("Bearer token", httpClient.DefaultRequestHeaders.GetValues("Authorization").First());
+            Assert.AreEqual($"projects/project1/datasets/dataset1/tables/events_{requestDatePartition}/data", job.RelativeUri);
+            Assert.AreEqual("10", job.ExecutionParameters[ExecutionParameterKeys.GoogleBigQueryApi.LASTINDEX]);
+            Assert.AreEqual("10", job.ExecutionParameters[ExecutionParameterKeys.GoogleBigQueryApi.TOTALROWS]);
+
+            repository.VerifyAll();
         }
 
         #region Helpers
