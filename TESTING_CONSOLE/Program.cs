9cfab983998f4e7a7d5a8f48f5fbd1a642b768d0
Jered Gosse <072984@sentry.com>
2022-09-21 12:38:19 -0500

    CLA-4412 - removing unused using statements

--- TESTING_CONSOLE/Program.cs
+++ TESTING_CONSOLE/Program.cs
@@ -1,15 +1,6 @@
-﻿using Microsoft.Win32;
-using Sentry.Common.Logging;
-using Sentry.Configuration;
-using Sentry.data.Core;
+﻿using Sentry.Common.Logging;
 using Sentry.data.Infrastructure;
-using StructureMap;
 using System;
-using System.Collections.Generic;
-using System.Data.Odbc;
-using System.Linq;
-using System.Text;
-using System.Threading;
 
 namespace TESTING_CONSOLE
 {
