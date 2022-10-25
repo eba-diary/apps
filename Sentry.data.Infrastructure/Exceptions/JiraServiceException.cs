394606a8fcb3eb14688fd2310efb094be31742f9
Sam Smith <Samuel.Smith@sentry.com>
2022-10-25 13:13:09 -0500

    CLA-4621: Iserializable

--- Sentry.data.Infrastructure/Exceptions/JiraServiceException.cs
+++ Sentry.data.Infrastructure/Exceptions/JiraServiceException.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sentry.data.Infrastructure.Exceptions
 {
     [Serializable]
-    public class JiraServiceException : Exception
+    public class JiraServiceException : Exception, ISerializable
     {
         public JiraServiceException(string message) : base(message)
         {
