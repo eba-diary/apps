9176e2f81f43c692c360d17f8dd50b7599cd43f1
Arnav Karnik <086642@sentry.com>
2022-12-06 11:20:35 -0600

    added a model that will be utilized in the controller to organize the service methods

---
+++ Sentry.data.Web/Models/AdminPage/ElasticSearchFileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sentry.data.Web.Models.AdminPage
+{
+    public class ElasticSearchFileModel
+    {
+        public int CompletedFiles { get; set; }
+
+        public int InFlightFiles { get; set; }
+
+        public int FailedFiles { get; set; }
+    }
+}
