6e4fdcd0976f184beafec1784c795dbd3224778d
Sam Smith <Samuel.Smith@sentry.com>
2022-12-07 08:03:18 -0600

    CLA-4483: Add try catch

--- Sentry.data.Core/DomainServices/DataSourceService.cs
+++ Sentry.data.Core/DomainServices/DataSourceService.cs
@@ -1,13 +1,14 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Sentry.Common.Logging;
 using Sentry.data.Core.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
+
 using static Sentry.data.Core.GlobalConstants;
 
 namespace Sentry.data.Core
@@ -20,6 +21,7 @@ namespace Sentry.data.Core
         private readonly IHttpClientProvider _httpClient;
         #endregion
 
+        #region Constructor
         public DataSourceService(IDatasetContext datasetContext,
                             IEncryptionService encryptionService,
                             IHttpClientProvider httpClient)
@@ -28,6 +30,7 @@ namespace Sentry.data.Core
             _encryptionService = encryptionService;
             _httpClient = httpClient;
         }
+        #endregion
 
         #region IDataSourceService Implementations
         public List<DataSourceTypeDto> GetDataSourceTypeDtosForDropdown()
@@ -95,21 +98,27 @@ namespace Sentry.data.Core
             };
             var jsonContent = JsonConvert.SerializeObject(content);
             var jsonPostContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-            var response = await _httpClient.PostAsync("https://api.gomotive.com/oauth/token", jsonPostContent);
-            JObject responseAsJson = JObject.Parse(await response.Content.ReadAsStringAsync());
-            string accessToken = responseAsJson.Value<string>("access_token");
-            string refreshToken = responseAsJson.Value<string>("refresh_token");
-            ((HTTPSSource)dataSource).Tokens.Add(new DataSourceToken()
+            try
             {
-                CurrentToken = accessToken,
-                RefreshToken = refreshToken,
-                TokenExp = 7200,
-                TokenUrl = "https://keeptruckin.com/oauth/token?grant_type=refresh_token&refresh_token=refreshtoken&redirect_uri=https://webhook.site/27091c3b-f9d0-42a2-a0d0-51b5134ac128&client_id=clientid&client_secret=clientsecret"
-                //company name as token name once we start dropping comapny file.
-            });
-            _datasetContext.SaveChanges();
+                var response = await _httpClient.PostAsync("https://api.gomotive.com/oauth/token", jsonPostContent);
+                JObject responseAsJson = JObject.Parse(await response.Content.ReadAsStringAsync());
+                string accessToken = responseAsJson.Value<string>("access_token");
+                string refreshToken = responseAsJson.Value<string>("refresh_token");
+                ((HTTPSSource)dataSource).Tokens.Add(new DataSourceToken()
+                {
+                    CurrentToken = accessToken,
+                    RefreshToken = refreshToken,
+                    TokenExp = 7200,
+                    TokenUrl = "https://keeptruckin.com/oauth/token?grant_type=refresh_token&refresh_token=refreshtoken&redirect_uri=https://webhook.site/27091c3b-f9d0-42a2-a0d0-51b5134ac128&client_id=clientid&client_secret=clientsecret"
+                    //company name as token name once we start dropping comapny file.
+                });
+                _datasetContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Logger.Fatal($"Token exchanged failed with Auth Token {authToken}. Exception {e.Message}.");
+            }
         }
-
 
         public List<AuthenticationTypeDto> GetAuthenticationTypeDtos()
         {
@@ -117,6 +126,7 @@ namespace Sentry.data.Core
             List<AuthenticationTypeDto> authenticationTypeDtos = allAuthTypes.Select(x => x.ToDto()).ToList();
             return authenticationTypeDtos;
         }
+
         #endregion
     }
 }
