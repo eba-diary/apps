b523a5bd5bfa6d1391d5ca276cb82d7e13af1b36
Caden Untiedt <082116@sentry.com>
2023-02-16 16:32:47 -0600

    add fluent validation methods in ValidationResponseModel, start implementation of Validate for AddDatasetRequestValidator

--- Sentry.data.Web/API/Dataset/AddDataset/AddDatasetRequestValidator.cs
+++ Sentry.data.Web/API/Dataset/AddDataset/AddDatasetRequestValidator.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using Sentry.data.Core;
+using System.Linq;
+using static Sentry.data.Core.GlobalConstants;
 
 namespace Sentry.data.Web.API
 {
     public class AddDatasetRequestValidator : IRequestModelValidator<AddDatasetRequestModel>
     {
+        private readonly IDatasetContext _datasetContext;
+
+        public AddDatasetRequestValidator(IDatasetContext datasetContext)
+        {
+            _datasetContext = datasetContext;
+        }
+
         public ValidationResponseModel Validate(AddDatasetRequestModel requestModel)
         {
-            throw new NotImplementedException();
+            ValidationResponseModel validationResponse = new ValidationResponseModel();
+
+            validationResponse.Requires(() => requestModel.DatasetName)
+                .Requires(() => requestModel.DatasetDescription)
+                .Requires(() => requestModel.ShortName)
+                .MaxLength(() => requestModel.ShortName, 12)
+                .RegularExpression(() => requestModel.ShortName, "^[0-9a-zA-Z]*$", "Only alphanumeric characters are allowed")
+                .Requires(() => requestModel.SaidAssetCode)
+                .Requires(() => requestModel.CategoryName)
+                .Requires(() => requestModel.OriginationCode)
+                .Requires(() => requestModel.DataClassificationTypeCode)
+                .Requires(() => requestModel.OriginalCreator)
+                .Requires(() => requestModel.NamedEnvironment)
+                .RegularExpression(() => requestModel.NamedEnvironment, "^[A-Z0-9]{1,10}$", "Must be alphanumeric, all caps, and less than 10 characters")
+                .Requires(() => requestModel.NamedEnvironmentTypeCode)
+                .MaxLength(() => requestModel.UsageInformation, 4096);
+
+            //dataset name exists
+            if (!string.IsNullOrWhiteSpace(requestModel.DatasetName) && !string.IsNullOrWhiteSpace(requestModel.NamedEnvironment) &&
+                _datasetContext.Datasets.Any(w => w.DatasetName.ToLower() == requestModel.DatasetName.ToLower() && w.DatasetType == DataEntityCodes.DATASET && w.NamedEnvironment == requestModel.NamedEnvironment))
+            {
+                validationResponse.AddFieldValidation(nameof(requestModel.DatasetName), "Dataset name already exists for the named environment");
+            }
+
+            //check code values
+            //data classification valid value
+            //dataset origination code valid value
+            //named environment type valid value
+
+            //check SAID asset
+            //look up asset
+            //verify the environment is valid for the asset and environment type matches
+
+            return validationResponse;
         }
 
         public ValidationResponseModel Validate(IRequestModel requestModel)
--- Sentry.data.Web/API/Validation/ValidationResponseModel.cs
+++ Sentry.data.Web/API/Validation/ValidationResponseModel.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace Sentry.data.Web.API
 {
@@ -13,6 +18,11 @@ namespace Sentry.data.Web.API
             return FieldValidations?.Any() == false;
         }
 
+        /// <summary>
+        /// Adds validation message for a field. If field already has validation, adds the message to the existing field validation.
+        /// </summary>
+        /// <param name="field"></param>
+        /// <param name="message"></param>
         public void AddFieldValidation(string field, string message)
         {
             if (FieldValidations?.Any() == false)
@@ -33,6 +43,40 @@ namespace Sentry.data.Web.API
             {
                 fieldValidation.AddValidationMessage(message);
             }
+        }
+
+        public ValidationResponseModel Requires(Expression<Func<string>> getPropertyValue)
+        {
+            if (string.IsNullOrWhiteSpace(getPropertyValue.Compile()()))
+            {
+                MemberExpression memberExp = getPropertyValue.Body as MemberExpression;
+                AddFieldValidation(memberExp.Member.Name, $"Required field");
+            }
+
+            return this;
+        }
+
+        public ValidationResponseModel MaxLength(Expression<Func<string>> getPropertyValue, int maxLength)
+        {
+            if (getPropertyValue.Compile()()?.Length > maxLength)
+            {
+                MemberExpression memberExp = getPropertyValue.Body as MemberExpression;
+                AddFieldValidation(memberExp.Member.Name, $"Max length of {maxLength} characters");
+            }
+
+            return this;
+        }
+
+        public ValidationResponseModel RegularExpression(Expression<Func<string>> getPropertyValue, string pattern, string message)
+        {
+            string value = getPropertyValue.Compile()();
+            if (!string.IsNullOrEmpty(value) && !Regex.IsMatch(getPropertyValue.Compile()(), pattern))
+            {
+                MemberExpression memberExp = getPropertyValue.Body as MemberExpression;
+                AddFieldValidation(memberExp.Member.Name, message);
+            }
+
+            return this;
         }
     }
 }
