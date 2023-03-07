cbe792b28c33bc6a59895fb009b2d3a9cbe764b4
Jered Gosse <072984@sentry.com>
2023-03-07 09:44:00 -0600

    CLA-4998 - Refactored config item and GetGroupName() logic - Added unit test

--- Sentry.data.Core/DTO/Security/AdSecurityGroupDto.cs
+++ Sentry.data.Core/DTO/Security/AdSecurityGroupDto.cs
@@ -47,9 +47,10 @@ namespace Sentry.data.Core.DTO.Security
 
         public string GetGroupName()
         {
-            if (bool.Parse(Config.GetHostSetting("IncludeNamedEnvironmentInAdGroup"))) //if nonprod, return add env to ticket 
+            if (!String.IsNullOrWhiteSpace(Config.GetHostSetting("AdGroupDSCEnvironmentIndicator"))) //if nonprod, return add env to ticket 
             {
-                return $"DS_{Config.GetDefaultEnvironmentName()}_{SaidAssetCode}_{DatasetShortName}_{GroupType}_{EnvironmentType}";
+                //return $"DS_{Config.GetDefaultEnvironmentName()}_{SaidAssetCode}_{DatasetShortName}_{GroupType}_{EnvironmentType}";
+                return $"DS_{SaidAssetCode}_{DatasetShortName}_{GroupType}_{Config.GetHostSetting("AdGroupDSCEnvironmentIndicator")}";
             }
             return $"DS_{SaidAssetCode}_{DatasetShortName}_{GroupType}_{EnvironmentType}";
         }
