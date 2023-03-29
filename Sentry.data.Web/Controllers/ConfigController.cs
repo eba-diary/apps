061cc25946a5ae0721882418aacad3b16e6c1d41
Sam Smith <Samuel.Smith@sentry.com>
2023-03-29 14:57:31 -0500

    CLA-4995: Check dataset security, update unauth page to forbidden page that has access guide and newer style

--- Sentry.data.Web/Controllers/ConfigController.cs
+++ Sentry.data.Web/Controllers/ConfigController.cs
@@ -111,16 +111,20 @@ namespace Sentry.data.Web.Controllers
             }
             else
             {
-                return View("Unauthorized");
+                return View("Forbidden");
             }
         }
 
         [HttpGet]
         [Route("Config/Dataset/{id}/Create")]
-        [AuthorizeByPermission(GlobalConstants.PermissionCodes.CAN_MANAGE_SCHEMA)]
         public ActionResult Create(int id)
         {
-            return View(GetDatasetFileConfigsModel(id));
+            var userSecurity = _DatasetService.GetUserSecurityForDataset(id);
+            if (userSecurity.CanManageSchema)
+            {
+                return View(GetDatasetFileConfigsModel(id));
+            }
+            return View("Forbidden");
         }
 
         [Route("Config/Dataset/ShowFileUpload/{configId}")]
@@ -135,10 +139,14 @@ namespace Sentry.data.Web.Controllers
         }
 
         [HttpGet]
-        [AuthorizeByPermission(GlobalConstants.PermissionCodes.CAN_MANAGE_SCHEMA)]
         public PartialViewResult _DatasetFileConfigCreate(int id)
         {
-            return PartialView("_DatasetFileConfigCreate", GetDatasetFileConfigsModel(id));
+            var userSecurity = _DatasetService.GetUserSecurityForDataset(id);
+            if (userSecurity.CanManageSchema)
+            {
+                return PartialView("_DatasetFileConfigCreate", GetDatasetFileConfigsModel(id));
+            }
+            return PartialView("Forbidden");
         }
 
         [HttpPost]
@@ -240,7 +248,7 @@ namespace Sentry.data.Web.Controllers
                 //Users are unable to edit 
                 if (!us.CanManageSchema || dto.DeleteInd)
                 {
-                    return View("Unauthorized");
+                    return View("Forbidden");
                 }
 
                 DatasetFileConfigsModel dfcm = new DatasetFileConfigsModel(dto);
@@ -738,7 +746,7 @@ namespace Sentry.data.Web.Controllers
 
                 if (!us.CanManageSchema || configDto.DeleteInd)
                 {
-                    return View("Unauthorized");
+                    return View("Forbidden");
                 }
 
                 if (configDto.Schema.SchemaId == schemaId)
@@ -765,7 +773,7 @@ namespace Sentry.data.Web.Controllers
             }
             catch (SchemaUnauthorizedAccessException)
             {
-                return View("Unauthorized");
+                return View("Forbidden");
             }
         }
 
@@ -894,7 +902,7 @@ namespace Sentry.data.Web.Controllers
 
                 if (!us.CanManageSchema || config.DeleteInd)
                 {
-                    return View("Unauthorized");
+                    return View("Forbidden");
                 }
 
                 FileSchemaDto schema = (config.Schema.SchemaId == schemaId) ? config.Schema : null;
@@ -925,7 +933,7 @@ namespace Sentry.data.Web.Controllers
             }
             catch (SchemaUnauthorizedAccessException)
             {
-                return View("Unauthorized");
+                return View("Forbidden");
             }
         }
 
@@ -937,7 +945,7 @@ namespace Sentry.data.Web.Controllers
 
             if (!us.CanManageSchema)
             {
-                return View("Unauthorized");
+                return View("Forbidden");
             }
 
             FileSchemaDto fileDto = _schemaService.GetFileSchemaDto(schemaId);
@@ -955,7 +963,7 @@ namespace Sentry.data.Web.Controllers
             }
             catch (SchemaUnauthorizedAccessException)
             {
-                return View("Unauthorized");
+                return View("Forbidden");
             }
             catch (Sentry.Core.ValidationException ex)
             {
@@ -1092,13 +1100,13 @@ namespace Sentry.data.Web.Controllers
             Task.Factory.StartNew(() => Utilities.CreateEventAsync(e), TaskCreationOptions.LongRunning);
         }
 
-        private DatasetFileConfigsModel GetDatasetFileConfigsModel(int id)
+        private DatasetFileConfigsModel GetDatasetFileConfigsModel(int datasetId)
         {
-            Dataset parent = _datasetContext.GetById<Dataset>(id);
+            Dataset parent = _datasetContext.GetById<Dataset>(datasetId);
 
             DatasetFileConfigsModel dfcm = new DatasetFileConfigsModel
             {
-                DatasetId = id,
+                DatasetId = datasetId,
                 ParentDatasetName = parent.DatasetName,
                 ObjectStatus = Core.GlobalEnums.ObjectStatusEnum.Active
             };
