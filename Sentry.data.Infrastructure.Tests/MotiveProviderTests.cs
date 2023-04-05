fa802e41a353d53a70c4f61bf948dd75bbee530d
Sam Smith <Samuel.Smith@sentry.com>
2023-04-05 15:56:05 -0500

    CLA-4484: Finish tests

--- Sentry.data.Infrastructure.Tests/MotiveProviderTests.cs
+++ Sentry.data.Infrastructure.Tests/MotiveProviderTests.cs
@@ -143,11 +143,12 @@ namespace Sentry.data.Infrastructure.Tests
                 SourceAuthType = new OAuthAuthentication(),
                 AllTokens = new List<DataSourceToken> { token0, token, token2, token3, token4, token5, token6, backfillToken }
             };
-
+            Dataset mockMotiveDataset = new Dataset { DatasetId = 7015 };
             List<DatasetFileConfig> mockFileConfigList = new List<DatasetFileConfig>
             {
                 new DatasetFileConfig
                 {
+                    ParentDataset = mockMotiveDataset,
                     Schema = new FileSchema
                     {
                         SchemaId = 23
@@ -155,6 +156,7 @@ namespace Sentry.data.Infrastructure.Tests
                 },
                 new DatasetFileConfig
                 {
+                    ParentDataset = mockMotiveDataset,
                     Schema = new FileSchema
                     {
                         SchemaId = 25
@@ -162,6 +164,7 @@ namespace Sentry.data.Infrastructure.Tests
                 },
                 new DatasetFileConfig
                 {
+                    ParentDataset = mockMotiveDataset,
                     Schema = new FileSchema
                     {
                         SchemaId = 26
@@ -186,6 +189,10 @@ namespace Sentry.data.Infrastructure.Tests
                         VariableName = "dateValue",
                         VariableValue = "2023-03-04"
                     }
+                },
+                DataFlow = new DataFlow
+                {
+                    Name = "Unit Test 1"
                 }
             };
 
@@ -204,6 +211,10 @@ namespace Sentry.data.Infrastructure.Tests
                         VariableName = "dateValue",
                         VariableValue = "2023-03-08"
                     }
+                },
+                DataFlow = new DataFlow
+                {
+                    Name = "Unit Test 2"
                 }
             };
 
@@ -222,6 +233,10 @@ namespace Sentry.data.Infrastructure.Tests
                         VariableName = "dateValue",
                         VariableValue = "2023-03-10"
                     }
+                },
+                DataFlow = new DataFlow
+                {
+                    Name = "Unit Test 3"
                 }
             };
 
@@ -269,7 +284,157 @@ namespace Sentry.data.Infrastructure.Tests
         [TestMethod]
         public void MotiveTokenBackfill_Unexpected()
         {
+            DataSourceToken token0 = new DataSourceToken { Id = 2, Enabled = false };
+            DataSourceToken token = new DataSourceToken { Id = 3, Enabled = true };
+            DataSourceToken token2 = new DataSourceToken { Id = 4, Enabled = true };
+            DataSourceToken token3 = new DataSourceToken { Id = 5, Enabled = false };
+            DataSourceToken token4 = new DataSourceToken { Id = 6, Enabled = true };
+            DataSourceToken token5 = new DataSourceToken { Id = 7, Enabled = true };
+            DataSourceToken token6 = new DataSourceToken { Id = 8, Enabled = false };
+            DataSourceToken backfillToken = new DataSourceToken { Id = 9, Enabled = false, BackfillComplete = false };
 
+            HTTPSSource dataSource = new HTTPSSource
+            {
+                BaseUri = new Uri("https://www.base.com"),
+                SourceAuthType = new OAuthAuthentication(),
+                AllTokens = new List<DataSourceToken> { token0, token, token2, token3, token4, token5, token6, backfillToken }
+            };
+            Dataset mockMotiveDataset = new Dataset { DatasetId = 7015 };
+            List<DatasetFileConfig> mockFileConfigList = new List<DatasetFileConfig>
+            {
+                new DatasetFileConfig
+                {
+                    ParentDataset = mockMotiveDataset,
+                    Schema = new FileSchema
+                    {
+                        SchemaId = 23
+                    }
+                },
+                new DatasetFileConfig
+                {
+                    ParentDataset = mockMotiveDataset,
+                    Schema = new FileSchema
+                    {
+                        SchemaId = 25
+                    }
+                },
+                new DatasetFileConfig
+                {
+                    ParentDataset = mockMotiveDataset,
+                    Schema = new FileSchema
+                    {
+                        SchemaId = 26
+                    }
+                },
+            };
+
+            List<int> mockSchemaIdList = new List<int> { 23, 25, 26 };
+
+            RetrieverJob job1 = new RetrieverJob
+            {
+                FileSchema = new FileSchema
+                {
+                    SchemaId = 23,
+                    CreateCurrentView = false
+                },
+                IsEnabled = true,
+                RequestVariables = new List<RequestVariable>
+                {
+                    new RequestVariable
+                    {
+                        VariableName = "dateValue",
+                        VariableValue = "2023-03-04"
+                    }
+                },
+                DataFlow = new DataFlow
+                {
+                    Name = "Unit Test 1"
+                }
+            };
+
+            RetrieverJob job2 = new RetrieverJob
+            {
+                FileSchema = new FileSchema
+                {
+                    SchemaId = 25,
+                    CreateCurrentView = false
+                },
+                IsEnabled = true,
+                RequestVariables = new List<RequestVariable>
+                {
+                    new RequestVariable
+                    {
+                        VariableName = "startValue",
+                        VariableValue = "2023-03-08"
+                    }
+                },
+                DataFlow = new DataFlow
+                {
+                    Name = "Unit Test 2"
+                }
+            };
+
+            RetrieverJob job3 = new RetrieverJob
+            {
+                FileSchema = new FileSchema
+                {
+                    SchemaId = 26,
+                    CreateCurrentView = false
+                },
+                IsEnabled = true,
+                RequestVariables = new List<RequestVariable>
+                {
+                    new RequestVariable
+                    {
+                        VariableName = "endValue",
+                        VariableValue = "2023-03-10"
+                    }
+                },
+                DataFlow = new DataFlow
+                {
+                    Name = "Unit Test 3"
+                }
+            };
+
+            MockRepository repository = new MockRepository(MockBehavior.Strict);
+
+            Mock<IDatasetContext> datasetContext = repository.Create<IDatasetContext>();
+            datasetContext.Setup(x => x.GetById<HTTPSSource>(It.IsAny<int>())).Returns(dataSource);
+
+            datasetContext.SetupGet(x => x.DatasetFileConfigs).Returns(mockFileConfigList.AsQueryable());
+
+            datasetContext.SetupGet(x => x.Jobs).Returns(new List<RetrieverJob> { job1, job2, job3 }.AsQueryable());
+
+            datasetContext.Setup(x => x.SaveChanges(true));
+
+            Mock<IBaseJobProvider> jobProvider = new Mock<IBaseJobProvider>();
+            jobProvider.Setup(x => x.Execute(It.IsAny<RetrieverJob>()));
+
+            MotiveProvider motiveProvider = new MotiveProvider(null, datasetContext.Object, null, null, jobProvider.Object);
+
+            var result = motiveProvider.MotiveTokenBackfill(backfillToken);
+
+            //backfill marked complete
+            Assert.IsFalse(backfillToken.BackfillComplete);
+
+            //all token state is back  to what it should be
+            Assert.IsFalse(token0.Enabled);
+            Assert.IsTrue(token.Enabled);
+            Assert.IsTrue(token2.Enabled);
+            Assert.IsFalse(token3.Enabled);
+            Assert.IsTrue(token4.Enabled);
+            Assert.IsTrue(token5.Enabled);
+            Assert.IsFalse(token6.Enabled);
+            Assert.IsTrue(backfillToken.Enabled);
+
+            //jobs set back to right place 
+            Assert.AreEqual(job1.RequestVariables.First(rv => rv.VariableName == "dateValue").VariableValue, "2023-03-04");
+            Assert.AreEqual(job2.RequestVariables.First(rv => rv.VariableName == "startValue").VariableValue, "2023-03-08");
+            Assert.AreEqual(job3.RequestVariables.First(rv => rv.VariableName == "endValue").VariableValue, "2023-03-10");
+
+            jobProvider.Verify(jp => jp.Execute(It.IsAny<RetrieverJob>()), Times.Exactly(1));
+
+            repository.VerifyAll();
         }
     }
 }
