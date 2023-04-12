706b47242bca22e7889c39faac527aae88bb7fdf
Jered Gosse <072984@sentry.com>
2023-04-12 09:20:18 -0500

    CLA-4243 - Rhino.Mocks was an unused reference - Removed files with no tests

---
+++ Sentry.data.Web.Tests/ConfigTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sentry.data.Web;
+using Sentry.data.Web.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Sentry.data.Web.Tests
+{
+    [TestClass]
+    public class ConfigTests
+    {
+
+        [TestMethod]
+        [TestCategory("Config Controller")]
+        public void Config_Controller_Index_Returns_Create_View()
+        {
+            var user = MockUsers.App_DataMgmt_Upld();
+
+            var ds = MockClasses.MockDataset();
+            var dfc = MockClasses.MockDatasetFileConfig(ds);
+
+            ds.DatasetFileConfigs.Add(dfc);
+
+            var cc = MockControllers.MockConfigController(dfc, user.Object);
+
+            var result = cc.Create(dfc.ParentDataset.DatasetId) as ViewResult;
+
+            Assert.IsInstanceOfType(result.Model, typeof(DatasetFileConfigsModel));
+
+            Assert.AreEqual("", result.ViewName);
+        }
+    }
+}
