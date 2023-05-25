5c9383a92fa41436e27c9cfea41e0562afeb22f2
Sam Smith <Samuel.Smith@sentry.com>
2023-05-25 13:31:47 -0500

    CLA-5098: Add tests and some tweaks

--- Sentry.data.Infrastructure/ServiceImplementations/MetadataMonitorService.cs
+++ Sentry.data.Infrastructure/ServiceImplementations/MetadataMonitorService.cs
@@ -10,32 +10,35 @@ namespace Sentry.data.Infrastructure
 {
     public class MetadataMonitorService : IMetadataMonitorService
     {
+        public MetadataMonitorService(IDatasetContext datasetContext, IEmailService emailService)
+        {
+            _datasetContext = datasetContext;
+            _emailService = emailService;
+        }
+
+        private IDatasetContext _datasetContext;
+        private IEmailService _emailService;
+
         public void CheckConsumptionLayerStatus()
         {
-            using (IContainer Container = Bootstrapper.Container.GetNestedContainer())
+            var schemas = _datasetContext.FileSchema.Where(s => !s.DeleteInd && s.Revisions.Any()).ToList();
+
+            List<SchemaConsumptionSnowflake> staleLayers = new List<SchemaConsumptionSnowflake>();
+
+            foreach (var schema in schemas)
             {
-                IDatasetContext _datasetContext = Container.GetInstance<IDatasetContext>();
-                IEmailService _emailService = Container.GetInstance<IEmailService>();
-
-                var schemas = _datasetContext.FileSchema.Where(s => !s.DeleteInd && s.Revisions.Any()).ToList();
-
-                List<SchemaConsumptionSnowflake> staleLayers = new List<SchemaConsumptionSnowflake>();
-
-                foreach (var schema in schemas)
+                foreach (var snowflakeConsumptionLayer in schema.ConsumptionDetails.OfType<SchemaConsumptionSnowflake>().Where(l => !(l.SnowflakeStatus == ConsumptionLayerTableStatusEnum.Available.ToString() || l.SnowflakeStatus == ConsumptionLayerTableStatusEnum.Deleted.ToString())).ToList())
                 {
-                    foreach(var snowflakeConsumptionLayer in schema.ConsumptionDetails.OfType<SchemaConsumptionSnowflake>().Where(l => !(l.SnowflakeStatus == ConsumptionLayerTableStatusEnum.Available.ToString() || l.SnowflakeStatus == ConsumptionLayerTableStatusEnum.Deleted.ToString())).ToList())
+                    if ((DateTime.Now - snowflakeConsumptionLayer.LastChanged).TotalHours > 24)
                     {
-                        if((DateTime.Now - snowflakeConsumptionLayer.LastChanged).TotalHours > 24)
-                        {
-                            staleLayers.Add(snowflakeConsumptionLayer);
-                        }
+                        staleLayers.Add(snowflakeConsumptionLayer);
                     }
                 }
+            }
 
-                if(staleLayers.Count > 0)
-                {
-                    _emailService.SendConsumptionLayerStaleEmail(staleLayers);
-                }
+            if (staleLayers.Count > 0)
+            {
+                _emailService.SendConsumptionLayerStaleEmail(staleLayers);
             }
         }
     }
